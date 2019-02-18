11f9a9386f111c78534fe38cb0210963ad2445fe
Gustavo <johnnygar98@hotmail.com>
2019-02-17 20:06:06 -0500

    Agregue las consultas a Lugar y tipo de persona

--- src/modelo/lugar/LugarBD.java
+++ src/modelo/lugar/LugarBD.java
@@ -1,19 +1,141 @@
 package modelo.lugar;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import modelo.ConectarDB;
+
 /**
  *
  * @author Johnny
  */
-public class LugarBD extends LugarMD{
+public class LugarBD extends LugarMD {
+
+    ConectarDB conecta = new ConectarDB();
 
     public LugarBD() {
     }
-    
-    public LugarMD buscar(int idLugar){
-        LugarBD lg = new LugarBD(); 
-        String sql = ""; 
-        
-        return lg; 
+
+    public LugarMD buscar(int idLugar) {
+        LugarBD lg = new LugarBD();
+        String sql = "SELECT id_lugar, lugar_codigo, lugar_nombre, lugar_nivel, "
+                + "id_lugar_referencia\n FROM public.\"Lugares\" "
+                + "WHERE id_lugar ='" + idLugar + "'; ";
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+                while (rs.next()) {
+                    lg.setCodigo(rs.getString("lugar_codigo"));
+                    lg.setId(rs.getInt("id_lugar"));
+                    lg.setIdReferencia(rs.getInt("id_lugar_referencia"));
+                    lg.setNivel(rs.getString("lugar_nivel"));
+                    lg.setNombre(rs.getString("lugar_nombre"));
+                }
+                return lg;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo consultar un lugar");
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
-    
+
+    public ArrayList<LugarMD> buscarPaises() {
+        ArrayList<LugarMD> lugares = new ArrayList();
+
+        String sql = "SELECT id_lugar, lugar_codigo, lugar_nombre, lugar_nivel, "
+                + "id_lugar_referencia\n FROM public.\"Lugares\" "
+                + "WHERE id_lugar_referencia IS NULL;";
+
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+                while (rs.next()) {
+                    LugarBD lg = new LugarBD();
+                    lg.setCodigo(rs.getString("lugar_codigo"));
+                    lg.setId(rs.getInt("id_lugar"));
+                    lg.setIdReferencia(rs.getInt("id_lugar_referencia"));
+                    lg.setNivel(rs.getString("lugar_nivel"));
+                    lg.setNombre(rs.getString("lugar_nombre"));
+
+                    lugares.add(lg);
+                }
+                return lugares;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo consultar lugares");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ArrayList<LugarMD> buscarPorReferencia(int idReferencia) {
+        ArrayList<LugarMD> lugares = new ArrayList();
+
+        String sql = "SELECT id_lugar, lugar_codigo, lugar_nombre, lugar_nivel, "
+                + "id_lugar_referencia\n FROM public.\"Lugares\" "
+                + "WHERE id_lugar_referencia = '" + idReferencia + "';";
+
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+                while (rs.next()) {
+                    LugarBD lg = new LugarBD();
+                    lg.setCodigo(rs.getString("lugar_codigo"));
+                    lg.setId(rs.getInt("id_lugar"));
+                    lg.setIdReferencia(rs.getInt("id_lugar_referencia"));
+                    lg.setNivel(rs.getString("lugar_nivel"));
+                    lg.setNombre(rs.getString("lugar_nombre"));
+
+                    lugares.add(lg);
+                }
+                return lugares;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo consultar lugares");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ArrayList<LugarMD> buscarPorNivel(int nivel) {
+        ArrayList<LugarMD> lugares = new ArrayList();
+
+        String sql = "SELECT id_lugar, lugar_codigo, lugar_nombre, lugar_nivel, "
+                + "id_lugar_referencia\n FROM public.\"Lugares\" "
+                + "WHERE lugar_nivel = '" + nivel + "';";
+
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+                while (rs.next()) {
+                    LugarBD lg = new LugarBD();
+                    lg.setCodigo(rs.getString("lugar_codigo"));
+                    lg.setId(rs.getInt("id_lugar"));
+                    lg.setIdReferencia(rs.getInt("id_lugar_referencia"));
+                    lg.setNivel(rs.getString("lugar_nivel"));
+                    lg.setNombre(rs.getString("lugar_nombre"));
+
+                    lugares.add(lg);
+                }
+                return lugares;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo consultar lugares");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
--- src/modelo/persona/TipoPersonaBD.java
+++ src/modelo/persona/TipoPersonaBD.java
@@ -1,26 +1,76 @@
 package modelo.persona;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import modelo.ConectarDB;
 
 /**
  *
  * @author Johnny
  */
-public class TipoPersonaBD extends TipoPersonaMD{
+public class TipoPersonaBD extends TipoPersonaMD {
+
+    ConectarDB conecta = new ConectarDB();
 
     public TipoPersonaBD() {
     }
-    
-    public ArrayList<TipoPersonaBD> cargarTipoPersona(){
-        ArrayList<TipoPersonaBD> tipos = new ArrayList();
-        
-        return tipos;
+
+    public ArrayList<TipoPersonaMD> cargarTipoPersona() {
+        ArrayList<TipoPersonaMD> tipos = new ArrayList();
+        String sql = "SELECT id_tipo_persona, tipo_persona\n"
+                + "FROM public.\"TipoPersona\" WHERE tipo_persona_activo = 'true';";
+
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+
+                while (rs.next()) {
+                    TipoPersonaMD tip = new TipoPersonaMD();
+                    tip.setId(rs.getInt("id_tipo_persona"));
+                    tip.setTipo(rs.getString("tipo_persona"));
+                    tipos.add(tip);
+                }
+                return tipos;
+            } else {
+                System.out.println("No se encontraron datos.");
+                return null;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("No se pudo consultar tipos de persona. ");
+            System.out.println(ex.getMessage());
+            return null;
+        }
     }
-    
-    public TipoPersonaMD buscar(int idTipoPersona){
-        TipoPersonaBD tipo = new TipoPersonaBD(); 
-        
-        return tipo; 
+
+    public TipoPersonaMD buscar(int idTipoPersona) {
+        TipoPersonaBD tipo = new TipoPersonaBD();
+        String sql = "SELECT id_tipo_persona, tipo_persona\n"
+                + "FROM public.\"TipoPersona\" WHERE tipo_persona_activo = 'true' "
+                + "and id_tipo_persona = '" + idTipoPersona + "';";
+
+        ResultSet rs = conecta.sql(sql);
+
+        try {
+            if (rs != null) {
+                TipoPersonaMD tip = new TipoPersonaMD();
+                while (rs.next()) {
+                    tip.setId(rs.getInt("id_tipo_persona"));
+                    tip.setTipo(rs.getString("tipo_persona"));
+                }
+                return tip;
+            } else {
+                System.out.println("No se encontraron datos.");
+                return null;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("No se pudo consultar tipos de persona. ");
+            System.out.println(ex.getMessage());
+            return null;
+        }
     }
-    
+
 }
