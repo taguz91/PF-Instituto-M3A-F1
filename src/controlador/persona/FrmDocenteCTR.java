7a8bf6edec3aedf6a21b486b8a961e1a2584c69b
paolaEs <46163863+paolaEs@users.noreply.github.com>
2019-03-10 16:33:12 -0500

    Commit Editar Docente
    Se edito el docente, ya esta el insertar, el editar, pero hay un detalle por consultar

--- src/controlador/persona/FrmDocenteCTR.java
+++ src/controlador/persona/FrmDocenteCTR.java
@@ -8,17 +8,19 @@ import modelo.persona.DocenteBD;
 import modelo.persona.DocenteMD;
 import vista.persona.FrmDocente;
 import vista.persona.FrmPersona;
+
 import vista.principal.VtnPrincipal;
 
-/**
- *
- * @author Johnny
- */
 public class FrmDocenteCTR {
 
+    //Para saber si se esta editando una persona  
+    private boolean editar = false;
+    private int idPersona = 0;
     private final VtnPrincipal vtnPrin;
+    private VtnDocenteCTR docenteVtn;
     private final FrmDocente frmDocente;
     private final DocenteBD docente;
+    private boolean guardar = false;
     private ArrayList<String> info = new ArrayList();
 
     //Para verificar si existe la persona tipo docente  
@@ -34,27 +36,42 @@ public class FrmDocenteCTR {
         frmDocente.show();
     }
 
-    public void persona(FrmPersona frmPersona) {
-        FrmPersonaCTR controladorPersona = new FrmPersonaCTR(vtnPrin, persona);
-        vtnPrin.getDpnlPrincipal().add(persona);
-        controladorPersona.iniciar();
-        frmPersona.show();
+    public FrmDocenteCTR(VtnPrincipal vtnPrin, FrmDocente frmDocente) {
+        this.vtnPrin = vtnPrin;
+        this.frmDocente = frmDocente;
+        //Inicializamos persona
+        this.docente = new DocenteBD();
+        vtnPrin.getDpnlPrincipal().add(frmDocente);
+        frmDocente.show();
+
+    }
+
+    private void abrirFrmPersona() {
+        FrmPersona frmPersona = new FrmPersona();
+        FrmPersonaCTR ctrFrmPersona = new FrmPersonaCTR(vtnPrin, frmPersona);
+        ctrFrmPersona.iniciar();
 
     }
 
     public void iniciar() {
+
         frmDocente.getBtnBuscarPersona().addActionListener(e -> buscarPersona(frmDocente.getTxtIdentificacion().getText()));
         frmDocente.getBtnGuardar().addActionListener(e -> guardarDocente());
+        //Accion de buscar una persona  
+
+        //   frmDocente.getBtnBuscarPersona().addActionListener(e -> consular());
     }
 
     public void guardarDocente() {
-        boolean guardar = true;
+        guardar = true;
         if (existeDocente) {
             docente.setIdPersona(Integer.parseInt(info.get(0)));
+            System.out.println(info.get(0) + "soy info{0} de guardar Docente en FRMDOCNETE");
         } else {
             guardar = false;
         }
         if (guardar) {
+
             docente.setCodigo(frmDocente.getTxtIdentificacion().getText());
             if (frmDocente.getCbxOtroTrabajo().isSelected()) {
                 docente.setDocenteOtroTrabajo(true);
@@ -62,15 +79,16 @@ public class FrmDocenteCTR {
                 docente.setDocenteOtroTrabajo(false);
             }
             docente.setDocenteCategoria(Integer.parseInt(frmDocente.getSpnCategoria().getValue().toString()));
-            String fechaInicio = frmDocente.getJdcFechaInicioContratacion().getText();
+            String fechaInicio = frmDocente.getJdcFechaInicioContratacion().getText().toUpperCase();
             String fecIni[] = fechaInicio.split("/");
             LocalDate fechaIni = LocalDate.of(Integer.parseInt(fecIni[2]),
                     Integer.parseInt(fecIni[1]), Integer.parseInt(fecIni[0]));
             docente.setFechaInicioContratacion(fechaIni);
-            String fecFinC[] = fechaInicio.split("/");
-            LocalDate fechaFin = LocalDate.of(Integer.parseInt(fecFinC[2]),
+            String fechaFin = frmDocente.getJdcFechaFinContratacion().getText();
+            String fecFinC[] = fechaFin.split("/");
+            LocalDate fechaFin1 = LocalDate.of(Integer.parseInt(fecFinC[2]),
                     Integer.parseInt(fecFinC[1]), Integer.parseInt(fecFinC[0]));
-            docente.setFechaFinContratacion(fechaFin);
+            docente.setFechaFinContratacion(fechaFin1);
             docente.setDocenteTipoTiempo(frmDocente.getCmbTipoTiempo().getSelectedItem().toString());
             if (frmDocente.getCbxDocenteCapacitador().isSelected()) {
                 docente.setDocenteCapacitador(true);
@@ -78,14 +96,17 @@ public class FrmDocenteCTR {
                 docente.setDocenteCapacitador(false);
             }
             docente.setEstado(null);
-            if (docente.InsertarDocente()) {
-                JOptionPane.showMessageDialog(null, "Datos guardados correctamente");
-
+            
+            
+            if (editar) {
+                if (idPersona > 0) {
+                    docente.editarDocente(idPersona);
+                    editar = false;
+                }
             } else {
-                JOptionPane.showMessageDialog(null, "Error al guardar");
+                docente.InsertarDocente();
+                JOptionPane.showMessageDialog(null, "Datos guardados correctamente");
             }
-
-            //  frmDocente.getBtnGuardar().addActionListener(e ->  docente.InsertarDocente());
         }
 
     }
@@ -102,56 +123,6 @@ public class FrmDocenteCTR {
             }
 
             try {
-                info = docente.existeDocente(identificacion);
-                if (info != null) {
-                    frmDocente.getLblDatosPersona().setForeground(Color.red);
-                    frmDocente.getLblDatosPersona().setText("Ya se encuentra dentro de la lista de docentes");
-                    frmDocente.getBtnRegistrarPersona().setVisible(false);
-                    inhabilitarComponentesDocente();
-                }
-
-            } catch (Exception w) {
-                      try {
-                info = docente.buscarPersonaDocente(identificacion);
-                if (info != null) {
-                    //Llenamos los datos en el lbl 
-                    frmDocente.getLblDatosPersona().setText(info.get(0) + " " + info.get(1));
-                    System.out.println("Numero de datos: " + info.size());
-                    habilitarComponentesDocente();
-                    //Indicamos que encontramos al docente  
-                    existeDocente = true;
-                }
-
-            } catch (Exception a) {
-                frmDocente.getLblDatosPersona().setForeground(Color.red);
-                frmDocente.getLblDatosPersona().setText("No se encuentra dentro de la lista Docentes");
-                frmDocente.getBtnRegistrarPersona().setVisible(false);
-                inhabilitarComponentesDocente();
-
-            }
-
-            }
-     
-        } catch (Exception e) {
-            frmDocente.getLblDatosPersona().setForeground(Color.red);
-            frmDocente.getLblDatosPersona().setText("No existe");
-            frmDocente.getBtnRegistrarPersona().setVisible(true);
-            frmDocente.getBtnRegistrarPersona().addActionListener(i -> persona(persona));
-            frmDocente.getBtnRegistrarPersona().setVisible(true);
-            inhabilitarComponentesDocente();
-
-        }
-
-        /*    try {
-            info = docente.buscarPersona(identificacion);
-            if (info != null) {
-                //Llenamos los datos en el lbl 
-                frmDocente.getLblDatosPersona().setForeground(Color.black);
-                frmDocente.getLblDatosPersona().setText(info.get(0) + " " + info.get(1));
-                frmDocente.getBtnRegistrarPersona().setVisible(false);
-                System.out.println("Numero de datos: " + info.size());
-            }
-            try {
                 info = docente.buscarPersonaDocente(identificacion);
                 if (info != null) {
                     //Llenamos los datos en el lbl 
@@ -173,21 +144,21 @@ public class FrmDocenteCTR {
             frmDocente.getLblDatosPersona().setForeground(Color.red);
             frmDocente.getLblDatosPersona().setText("No existe");
             frmDocente.getBtnRegistrarPersona().setVisible(true);
-            frmDocente.getBtnRegistrarPersona().addActionListener(i -> persona(persona));
+            frmDocente.getBtnRegistrarPersona().addActionListener(i -> abrirFrmPersona());
             frmDocente.getBtnRegistrarPersona().setVisible(true);
             inhabilitarComponentesDocente();
 
         }
-         */
+
     }
 
     public void habilitarComponentesDocente() {
+        frmDocente.getJdcFechaInicioContratacion().setEnabled(true);
         frmDocente.getBtnGuardar().setEnabled(true);
         frmDocente.getCbxDocenteCapacitador().setEnabled(true);
         frmDocente.getCmbTipoTiempo().setEnabled(true);
         frmDocente.getSpnCategoria().setEnabled(true);
         frmDocente.getJdcFechaFinContratacion().setEnabled(true);
-        frmDocente.getJdcFechaInicioContratacion().setEnabled(true);
         frmDocente.getCbxOtroTrabajo().setEnabled(true);
     }
 
@@ -199,5 +170,24 @@ public class FrmDocenteCTR {
         frmDocente.getJdcFechaFinContratacion().setEnabled(false);
         frmDocente.getJdcFechaInicioContratacion().setEnabled(false);
         frmDocente.getCbxOtroTrabajo().setEnabled(false);
+
     }
+
+    public void editar(DocenteMD doc) {
+        editar = true;
+        idPersona = doc.getIdPersona();
+        System.out.println("Id de la persona que editaremos " + idPersona);
+//        System.out.println(doc.getFechaInicioContratacion().format(DateTimeFormatter.ISO_DATE));
+//        System.out.println(doc.getFechaInicioContratacion().getDayOfMonth());
+      //  frmDocente.getJdcFechaFinContratacion().setText(doc.getFechaInicioContratacion().format(DateTimeFormatter.ISO_DATE));
+        frmDocente.getTxtIdentificacion().setText(doc.getCodigo());
+        frmDocente.getSpnCategoria().setValue(doc.getDocenteCategoria());
+        frmDocente.getCmbTipoTiempo().setSelectedItem(doc.getDocenteTipoTiempo());
+        frmDocente.getCbxDocenteCapacitador().setSelected(doc.isDocenteCapacitador());
+        frmDocente.getCbxOtroTrabajo().setSelected(doc.isDocenteOtroTrabajo());
+        habilitarComponentesDocente();
+        frmDocente.getTxtIdentificacion().setEnabled(false);
+        frmDocente.getBtnRegistrarPersona().setVisible(false);
+    }
+
 }
