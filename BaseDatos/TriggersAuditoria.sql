da5a3991c96e9c68f87c1c62ea050d4777eda7bc
Gustavo <johnnygar98@hotmail.com>
2019-03-12 16:24:51 -0500

    Me cambiare de rama para revisar los cambios hechos

---
+++ BaseDatos/TriggersAuditoria.sql
@@ -0,0 +1,11 @@
+--Funcion en la que guardaremos en la tabla usuarios
+CREATE OR REPLACE FUNCTION historial_acciones() RETURNS TRIGGER
+AS $historial_acciones$
+BEGIN
+	INSERT INTO public."HistorialUsuarios" (
+		usu_username, historial_fecha, historial_tipo_accion,
+		historial_nombre_tabla, historial_pk_tabla)
+	VALUES(USER, now(), TG_OP, TG_TABLE_NAME, new.id_persona);
+	RETURN NEW;
+END;
+$historial_acciones$ LANGUAGE plpgsql;  
