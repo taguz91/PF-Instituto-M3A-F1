97865f8946a7aa96d1af3c8ef0a03d51dbd05a5d
Gustavo <johnnygar98@hotmail.com>
2019-03-12 20:26:44 -0500

    Cotinuando con los triggers, debo consultar en que tablas va antes de continuar

--- BaseDatos/UpdatesMateriasHoras.sql
+++ BaseDatos/UpdatesMateriasHoras.sql
@@ -1,5 +1,4 @@
     --Primero
-	
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=18, materia_horas_auto_estudio=50, materia_horas_presencial=72, materia_total_horas=122
 	WHERE id_carrera = 2 and materia_nombre='MATEMÁTICA DISCRETA';
@@ -7,129 +6,123 @@
     UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=0, materia_horas_auto_estudio=40, materia_horas_presencial=54, materia_total_horas=94
 	WHERE id_carrera = 2 and materia_nombre='DESARROLLO DEL PENSAMIENTO';
-	
+
     UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=36, materia_horas_auto_estudio=40, materia_horas_presencial=90, materia_total_horas=130
 	WHERE id_carrera = 2 and materia_nombre='INTRODUCCIÓN AL DESARROLLO DE SOFTWARE';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72, materia_horas_practicas=54, materia_horas_auto_estudio=60, materia_horas_presencial=126, materia_total_horas=186
 	WHERE id_carrera = 2 and  materia_nombre='FUNDAMENTOS DE PROGRAMACIÓN';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72, materia_horas_practicas=36, materia_horas_auto_estudio=50, materia_horas_presencial=108, materia_total_horas=158
 	WHERE id_carrera = 2 and materia_nombre='ANÁLISIS Y DISEÑO DE SISTEMAS';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=36, materia_horas_auto_estudio=30, materia_horas_presencial=90, materia_total_horas=120
 	WHERE id_carrera = 2 and materia_nombre='INGLÉS A1(BÁSICO)';
-	
+
 	--Segundo
-	
+
 	 UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=18, materia_horas_auto_estudio=50, materia_horas_presencial=72, materia_total_horas=122
 	WHERE id_carrera = 2 and materia_nombre='ALGEBRA Y TRIGONOMETRÍA';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72, materia_horas_practicas=54, materia_horas_auto_estudio=70, materia_horas_presencial=126, materia_total_horas=196
 	WHERE id_carrera = 2 and materia_nombre='BASE DE DATOS';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72, materia_horas_practicas=54, materia_horas_auto_estudio=70, materia_horas_presencial=126, materia_total_horas=196
 	WHERE id_carrera = 2 and materia_nombre='PROGRAMACIÓN ORIENTADA A OBJETOS';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=18, materia_horas_auto_estudio=20, materia_horas_presencial=72, materia_total_horas=92
 	WHERE id_carrera = 2 and materia_nombre='METODOLOGÍAS DE DESARROLLO DE SOFTWARE';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=0, materia_horas_auto_estudio=30, materia_horas_presencial=54, materia_total_horas=84
 	WHERE id_carrera = 2 and materia_nombre='LENGUAJE Y COMUNICACIÓN';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54, materia_horas_practicas=36, materia_horas_auto_estudio=30, materia_horas_presencial=90, materia_total_horas=120
 	WHERE id_carrera = 2 and materia_nombre='INGLÉS A2(INTERMEDIO)';
-	
+
 	--Tercero
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=18,materia_horas_auto_estudio=50,materia_horas_presencial=72,materia_total_horas=122
 	WHERE id_carrera = 2 and materia_nombre = 'CALCULO DIFERENCIAL E INTEGRAL';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=0,materia_horas_auto_estudio=30,materia_horas_presencial=54,materia_total_horas=84
 	WHERE id_carrera = 2 and materia_nombre = 'FUNDAMENTOS DE ADMINISTRACIÓN';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=74,materia_horas_practicas=54,materia_horas_auto_estudio=70,materia_horas_presencial=128,materia_total_horas=196
 	WHERE id_carrera = 2 and materia_nombre = 'PROGRAMACIÓN VISUAL';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=36,materia_horas_auto_estudio=60,materia_horas_presencial=108,materia_total_horas=168
 	WHERE id_carrera = 2 and materia_nombre = 'BASE DE DATOS AVANZADA';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=36,materia_horas_auto_estudio=30,materia_horas_presencial=90,materia_total_horas=120
 	WHERE id_carrera = 2 and materia_nombre = 'DISEÑO DE INTERFAZ';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=36,materia_horas_auto_estudio=30,materia_horas_presencial=90,materia_total_horas=120
 	WHERE id_carrera = 2 and materia_nombre = 'INGLÉS B1.1(TECHNICAL)';
-	
+
 	--Cuarto
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=18,materia_horas_auto_estudio=30,materia_horas_presencial=72,materia_total_horas=102
 	WHERE id_carrera = 2 and materia_nombre = 'ESTADÍSTICA DESCRIPTIVA';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=0,materia_horas_auto_estudio=20,materia_horas_presencial=72,materia_total_horas=92
 	WHERE id_carrera = 2 and materia_nombre = 'LEGISLACIÓN INFORMÁTICA';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=72,materia_horas_auto_estudio=50,materia_horas_presencial=144,materia_total_horas=194
 	WHERE id_carrera = 2 and materia_nombre = 'PROGRAMACIÓN DE APLICACIONES  WEB';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=54,materia_horas_auto_estudio=50,materia_horas_presencial=126,materia_total_horas=176
 	WHERE id_carrera = 2 and materia_nombre = 'DESARROLLO DE APLICACIONES MÓVILES';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=36,materia_horas_auto_estudio=30,materia_horas_presencial=90,materia_total_horas=120
 	WHERE id_carrera = 2 and materia_nombre = 'INGLÉS B1.2(SPECIFIC PURPOSE)';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=36,materia_horas_practicas=0,materia_horas_auto_estudio=30,materia_horas_presencial=36,materia_total_horas=66
 	WHERE id_carrera = 2 and materia_nombre = 'DIVERSIDAD Y CULTURA';
-	
-	
+
+
 	--Quinto
-	
+
  UPDATE public."Materias"
 	SET materia_horas_docencia=54,materia_horas_practicas=18,materia_horas_auto_estudio=28,materia_horas_presencial=72,materia_total_horas=100
 	WHERE id_carrera = 2 and materia_nombre = 'EMPRENDIMIENTOS';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=36,materia_horas_practicas=0,materia_horas_auto_estudio=204,materia_horas_presencial=36,materia_total_horas=240
 	WHERE id_carrera = 2 and materia_nombre = 'PROYECTO DE TITULACIÓN';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=90,materia_horas_practicas=72,materia_horas_auto_estudio=40,materia_horas_presencial=162,materia_total_horas=202
 	WHERE id_carrera = 2 and materia_nombre = 'TENDENCIAS ACTUALES DE PROGRAMACIÓN';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=36,materia_horas_auto_estudio=40,materia_horas_presencial=108,materia_total_horas=148
 	WHERE id_carrera = 2 and materia_nombre = 'CALIDAD DEL SOFTWARE';
-	
+
 	UPDATE public."Materias"
 	SET materia_horas_docencia=72,materia_horas_practicas=54,materia_horas_auto_estudio=40,materia_horas_presencial=126,materia_total_horas=166
 	WHERE id_carrera = 2 and materia_nombre = 'FUNDAMENTOS DE REDES Y CONECTIVIDAD';
-	
+
 	 UPDATE public."Materias"
 	SET materia_horas_docencia=36,materia_horas_practicas=0,materia_horas_auto_estudio=28,materia_horas_presencial=36,materia_total_horas=64
 	WHERE id_carrera = 2 and materia_nombre = 'ÉTICA PROFESIONAL';
-
-	
-	
-	
-	
-	
