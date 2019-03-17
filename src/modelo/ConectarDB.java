6280104a0c95e2bc6604f73328f0437c911baf11
Taguz98 <gus199811@gmail.com>
2019-03-17 09:10:46 -0500

    Ningun cambio importante, solo por si tenga cambios aqui

--- src/modelo/ConectarDB.java
+++ src/modelo/ConectarDB.java
@@ -24,18 +24,18 @@ public class ConectarDB {
     //private String url = "jdbc:postgresql://localhost:5433/BDPFInstitutoM3A"; //BD Johnny PCPRO
     //private String url = "jdbc:postgresql://localhost:5433/BDPFINSTITUTO"; //BD Johnny PCPRO
     //private String url = "jdbc:postgresql://192.168.1.12:5432/Proyecto-Academico-Insta"; //BD Diego ServerLaptop
-    //private String url = "jdbc:postgresql://localhost:5432/BDPFInstitutoM3A"; //BD Johnny PCNOOB
-    private String url = "jdbc:postgresql://localhost:5432/BDPFCompletaPruebas"; //BD Lina
+    private String url = "jdbc:postgresql://localhost:5432/BDPFM3APruebas"; //BD Johnny PCNOOB
+    //private String url = "jdbc:postgresql://localhost:5432/BDPFCompletaPruebas"; //BD Lina
     //private String url = "jdbc:postgresql://localhost:5432/ingreso"; //BD PAO M
     //private String url = "jdbc:postgresql://localhost:5432/Proyecto"; // BD Andres Novillo
     //private String url = "jdbc:postgresql://localhost:5432/ingreso"; //BD PAO MEDINA
     private String user = "postgres";
 
-    //private String pass = "Holapostgres"; //Clave Johnny
+    private String pass = "Holapostgres"; //Clave Johnny
     //private String pass = ""; //Clave Diego
     //private String pass = "PAOLA"; //Clave Paola
     //private String pass = "NuEvOsErVeR1997"; // Clave Andres
-    private String pass = "linis4413"; //Clave Lina
+    //private String pass = "linis4413"; //Clave Lina
 
     public ConectarDB(String mensaje) {
         try {
