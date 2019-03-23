cf202066583781c478ebcf69e859776218f7b6e8
LinaOrellana <46226513+LinaOrellana@users.noreply.github.com>
2019-03-22 19:04:26 -0500

    Merge

--- src/modelo/ConectarDB.java
+++ src/modelo/ConectarDB.java
@@ -18,9 +18,9 @@ public class ConectarDB {
     private ResultSet rs;
     
     //Base de datos en la nube ... si no quiere conectarse a la nube comente esto
-    private String url = "jdbc:postgresql://35.193.226.187:5432/BDinsta";
-    private String user = "ROOT";
-    private String pass = "ROOT";
+//    private String url = "jdbc:postgresql://35.193.226.187:5432/BDinsta";
+//    private String user = "ROOT";
+//    private String pass = "ROOT";
 
     //private String url = "jdbc:postgresql://localhost:5433/BDPFM3AConCursados"; //BD Johnny PCPRO
     //private String url = "jdbc:postgresql://localhost:5433/BDPFM3A"; //BD Johnny PCPRO
@@ -30,18 +30,12 @@ public class ConectarDB {
     //private String url = "jdbc:postgresql://190.11.21.244:5432/BDinsta"; //BD Diego
     //private String url = "jdbc:postgresql://192.168.1.12:5432/Proyecto-Academico-Insta"; //BD Diego
 
-    //Base de datos en la nube ... si no quiere conectarse a la nube comente esto 
-    //jdbc:postgresql://35.193.226.187:5432/BDinsta
-//    private String url = "jdbc:postgresql://35.193.226.187:5432/BDinsta";
-//    private String user = "ROOT";
-//    private String pass = "ROOT";
-
     
     //private String url = "jdbc:postgresql://localhost:5433/BDPFM3AConMallaCursada"; //BD David
     //private String url = "jdbc:postgresql://localhost:5432/proyecto"; //BD David
 
     // private String url = "jdbc:postgresql://localhost:5432/PFcompleta"; //BD Lina
-
+    private String url = "jdbc:postgresql://localhost:5432/BDPFConAlumnosPorCurso"; //BD Lina
    // private String url = "jdbc:postgresql://localhost:5432/BDTESIS"; //BD Johnny PCNOOB
 
     //private String url = "jdbc:postgresql://localhost:5432/ingreso"; //BD PAO MEDINA
@@ -58,7 +52,7 @@ public class ConectarDB {
     //private String user = "ROOT";
 
     //private String user = "ROOT";
-    //private String user = "postgres";
+    private String user = "postgres";
     //private String url = "jdbc:postgresql://localhost:5432/Proyecto"; // BD Andres Novillo
     //  private String url = "jdbc:postgresql://localhost:5432/Pruebas"; //BD PAO MEDINA
     //private String user = "ROOT";//USUARIO DIEGO
@@ -76,20 +70,9 @@ public class ConectarDB {
     //private String pass = ""; //Clave Diego
     //private String pass = "PAOLA"; //Clave Paola
     //private String pass = "NuEvOsErVeR1997"; // Clave Andres
-
-    //private String pass = "linis4413"; //Clave Lina
-    public ConectarDB(String mensaje) {
-    }
-    //private String pass = "davicho"; // Clave David
-    //private String pass = "linis4413"; //Clave Lina*/
-    //Base de datos en la nube ... si no quiere conectarse a la nube comente esto
-    //private String url = "jdbc:postgresql://localhost:5432/BDinsta";
-    //private String url = "jdbc:postgresql://35.193.226.187:5432/BDinsta";//BD cloud
-//    private String user = "ROOT";
-//    private String pass = "ROOT";
-
+    private String pass = "linis4413"; //Clave Lina
+    
     public ConectarDB(String user, String pass, String mensaje) {
-
         try {
             //Cargamos el driver
             Class.forName("org.postgresql.Driver");
