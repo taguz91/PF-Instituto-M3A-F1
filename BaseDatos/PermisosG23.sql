94d79f4a3801ebc86ab87680e13fd9ad2d71b21e
paolaEs <46163863+paolaEs@users.noreply.github.com>
2019-03-25 20:22:33 -0500

    commit permisos

--- BaseDatos/PermisosG23.sql
+++ BaseDatos/PermisosG23.sql
@@ -5,3 +5,10 @@ INSERT INTO "Accesos" VALUES(40,'PeriodoLectivo-CerraPeriodo');
 INSERT INTO "Accesos" VALUES(41,'PeriodoLectivo-Editar');
 INSERT INTO "Accesos" VALUES(42,'PeriodoLectivo-Ingresar');
 
+INSERT INTO "Accesos" VALUES(43,'DOCENTES-Ingresar');
+INSERT INTO "Accesos" VALUES(44,'DOCENTES-Editar');
+INSERT INTO "Accesos" VALUES(45,'DOCENTES-Eliminar');
+INSERT INTO "Accesos" VALUES(46,'DOCENTES-Materias Docente');
+INSERT INTO "Accesos" VALUES(47,'DOCENTES-Docente');
+
+
