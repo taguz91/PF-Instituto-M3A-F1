aa529976d88196e5d89e2808818765df0551f9fa
LinaOrellana <46226513+LinaOrellana@users.noreply.github.com>
2019-03-26 18:09:30 -0500

    Merge

--- src/modelo/ResourceManager.java
+++ src/modelo/ResourceManager.java
@@ -16,10 +16,10 @@ public class ResourceManager {
 
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
 
-    private static final String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";//BD cloud
+   // private static final String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";//BD cloud
     //private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/Proyecto_Final";//BD Andres
 
-//    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/BDPFConAlumnosCurso";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/BDPFConAlumnosCurso";
 //    private static String USERNAME = "";
 //    private static String PASSWORD = "";
 
