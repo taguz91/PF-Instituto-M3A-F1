6d2e99008051de352d49b6da8ff25d14bea105c2
Gustavo <johnnygar98@hotmail.com>
2019-03-28 17:08:10 -0500

    Los triggers fueron ejecutados en la base ahora, hora de probarlos

--- src/modelo/ResourceManager.java
+++ src/modelo/ResourceManager.java
@@ -16,9 +16,7 @@ public class ResourceManager {
 
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
 
-    //private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/Proyecto_Final";//BD Andres
-    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/BDPFConAlumnosCurso";//BD Andres
-    //private static String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";//BD cloud
+    private static String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";//BD cloud
 
     private static String USERNAME = "ROOT";
     private static String PASSWORD = "ROOT";
