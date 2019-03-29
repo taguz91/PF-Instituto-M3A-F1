a339743b2e13dbbf59d9062cdad2c6f1c0806d83
MrRainx <40721962+diegocondo10@users.noreply.github.com>
2019-03-28 21:58:52 -0500

    solo es un push de pruebas

--- src/controlador/persona/FrmPersonaCTR.java
+++ src/controlador/persona/FrmPersonaCTR.java
@@ -732,7 +732,7 @@ public class FrmPersonaCTR {
             per.setSegundoNombre(segNombre);
             per.setPrimerApellido(priApellido);
             per.setSegundoApellido(segApellido);
-            per.setFechaNacimiento(FechaNac);
+            //per.setFechaNacimiento(FechaNac);
             per.setEstadoCivil(estadoCivil);
             per.setCelular(celular);
             per.setTelefono(telefono);
