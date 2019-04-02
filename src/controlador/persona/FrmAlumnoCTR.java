33eb1b192441ee59936d11397a5de7a6d635b696
Andres97 <15andresc@gmail.com>
2019-04-01 21:41:43 -0500

    Cambios :)

--- src/controlador/persona/FrmAlumnoCTR.java
+++ src/controlador/persona/FrmAlumnoCTR.java
@@ -336,10 +336,11 @@ public class FrmAlumnoCTR {
 
                 if (error == false) {
 
-                    AlumnoMD alumno = bdAlumno.buscarPersonaxCedula(frmAlumno.getTxt_Cedula().getText());
-                    PersonaMD p = per.buscarPersonaParaReferencia(frmAlumno.getTxt_Cedula().getText());
-                    //List<PersonaMD> p = bdAlumno.filtrarPersona(frmAlumno.getTxt_Cedula().getText());
-                    if (alumno.getIdentificacion() == null && p.getIdentificacion() == null) {
+                    //AlumnoMD alumno = bdAlumno.buscarPersonaxCedula(frmAlumno.getTxt_Cedula().getText());
+                    //PersonaMD p = per.buscarPersonaParaReferencia(frmAlumno.getTxt_Cedula().getText());
+                    List<PersonaMD> p = bdAlumno.filtrarPersona(frmAlumno.getTxt_Cedula().getText());
+                    //if (alumno.getIdentificacion() == null && p.getIdentificacion() == null) {
+                    if(p.get(0).getIdentificacion() == null){
                         int dialog = JOptionPane.YES_NO_CANCEL_OPTION;
                         int result = JOptionPane.showConfirmDialog(null, "Usted no esta registrado en el Sistema ¿DESEA HACERLO? ", " Registrar Persona ", dialog);
                         if (result == 0) {
@@ -356,11 +357,10 @@ public class FrmAlumnoCTR {
                         Integer idAlumno = alumno.getId_Alumno();
                         Font negrita = new Font("Tahoma", Font.BOLD, 13);
                         frmAlumno.getTxt_Nombre().setFont(negrita);
-//                        frmAlumno.getTxt_Nombre().setText(p.get(0).getPrimerNombre() + " " + p.get(0).getSegundoNombre()
-//                                + " " + p.get(0).getPrimerApellido() + " " + p.get(0).getSegundoApellido());
-                        frmAlumno.getTxt_Nombre().setText(p.getPrimerNombre() + " " + p.getSegundoNombre() + " " + 
-                                p.getPrimerApellido() + " " + p.getSegundoApellido());
-                        habilitarGuardar();
+                        frmAlumno.getTxt_Nombre().setText(p.get(0).getPrimerNombre() + " " + p.get(0).getSegundoNombre()
+                                + " " + p.get(0).getPrimerApellido() + " " + p.get(0).getSegundoApellido());
+//                        frmAlumno.getTxt_Nombre().setText(p.getPrimerNombre() + " " + p.getSegundoNombre() + " " + 
+//                                p.getPrimerApellido() + " " + p.getSegundoApellido());
                         if (idAlumno == null) {
 //                                frmAlumno.getTxt_Nombre().setText(alumno.getPrimerNombre() + " " + alumno.getSegundoNombre()
 //                                        + " " + alumno.getPrimerApellido() + " " + alumno.getSegundoApellido());
--- src/modelo/ResourceManager.java
+++ src/modelo/ResourceManager.java
@@ -16,12 +16,12 @@ public class ResourceManager {
 
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
 
-    private static String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";
+    //private static String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDinsta";
     //Esta base de datos es la que entrera en pruebas del dia de mañana no modificar nada
     //private static String JDBC_URL = "jdbc:postgresql://35.193.226.187:5432/BDpruebas";
     //private static String JDBC_URL = "jdbc:postgresql://localhost:5432/BDinsta";
     //private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/Proyecto_Final";//BD Andres
-    //private static final String JDBC_URL = "jdbc:postgresql://LocalHost:5432/BD_Final";
+    private static final String JDBC_URL = "jdbc:postgresql://LocalHost:5432/BD_Final"; //BD Andres
 
     private static String USERNAME = "ROOT";
     private static String PASSWORD = "ROOT";
