beabd164fdfba3fc857cfe397d5c20e32937a5aa
Andres97 <15andresc@gmail.com>
2019-04-01 21:20:34 -0500

    Cambios

--- src/controlador/persona/FrmAlumnoCTR.java
+++ src/controlador/persona/FrmAlumnoCTR.java
@@ -333,9 +333,8 @@ public class FrmAlumnoCTR {
 
                 if (error == false) {
 
-                    AlumnoMD alumno = bdAlumno.buscarPersonaxCedula(frmAlumno.getTxt_Cedula().getText());
-                    //List<PersonaMD> p = bdAlumno.filtrarPersona(frmAlumno.getTxt_Cedula().getText());
-                    if (alumno.getIdentificacion() == null) {
+                    List<PersonaMD> p = bdAlumno.filtrarPersona(frmAlumno.getTxt_Cedula().getText());
+                    if (p.get(0).getIdentificacion() == null) {
                         int dialog = JOptionPane.YES_NO_CANCEL_OPTION;
                         int result = JOptionPane.showConfirmDialog(null, "Usted no esta registrado en el Sistema ¿DESEA HACERLO? ", " Registrar Persona ", dialog);
                         if (result == 0) {
@@ -348,15 +347,16 @@ public class FrmAlumnoCTR {
                         }
                         cont = 0;
                     } else {
+                        AlumnoMD alumno = bdAlumno.buscarPersona(p.get(0).getIdPersona());
+                        Integer idAlumno = alumno.getId_Alumno();
                         Font negrita = new Font("Tahoma", Font.BOLD, 13);
                         frmAlumno.getTxt_Nombre().setFont(negrita);
 //                        frmAlumno.getTxt_Nombre().setText(p.get(0).getPrimerNombre() + " " + p.get(0).getSegundoNombre()
 //                                + " " + p.get(0).getPrimerApellido() + " " + p.get(0).getSegundoApellido());
-                        frmAlumno.getTxt_Nombre().setText(alumno.getPrimerNombre() + " " + alumno.getSegundoNombre() + " "
-                                + alumno.getPrimerApellido() + " " + alumno.getSegundoApellido());
+                        frmAlumno.getTxt_Nombre().setText(p.get(0).getPrimerNombre() + " " + p.get(0).getSegundoNombre() + " "
+                                + p.get(0).getPrimerApellido() + " " + p.get(0).getSegundoApellido());
                         habilitarGuardar();
-//                            System.out.println(p.get(0).getIdPersona());
-                        if (alumno.getTipo_Colegio() == null) {
+                        if (idAlumno == null) {
 //                                frmAlumno.getTxt_Nombre().setText(alumno.getPrimerNombre() + " " + alumno.getSegundoNombre()
 //                                        + " " + alumno.getPrimerApellido() + " " + alumno.getSegundoApellido());
                             cont = 0;
