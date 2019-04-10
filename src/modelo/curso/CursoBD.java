9eac504948f3ff366f419ef68c0263b7eb685d6a
Johnny <johnnygar98@hotmail.com>
2019-04-10 08:30:13 -0500

    Cursos ahora muestra en la tabla en que periodo fue

--- src/modelo/curso/CursoBD.java
+++ src/modelo/curso/CursoBD.java
@@ -77,15 +77,17 @@ public class CursoBD extends CursoMD {
         String sql = "SELECT id_curso, materia_nombre, \n"
                 + "persona_primer_nombre, persona_primer_apellido, \n"
                 + "curso_nombre, curso_capacidad, curso_ciclo, \n"
-                + "c.id_prd_lectivo, c.id_materia, c.id_docente\n"
+                + "c.id_prd_lectivo, c.id_materia, c.id_docente, \n"
+                + "prd_lectivo_nombre \n"
                 + "FROM public.\"Cursos\" c, public.\"Materias\" m, \n"
                 + "public.\"Docentes\" d, public.\"Personas\" p, \n"
-                + "public.\"Carreras\" cr\n"
+                + "public.\"PeriodoLectivo\" pl \n"
                 + "WHERE m.id_materia = c.id_materia AND \n"
-                + "cr.id_carrera = m.id_carrera AND\n"
                 + "d.id_docente = c.id_docente AND \n"
                 + "p.id_persona = d.id_persona AND persona_activa = true AND\n"
-                + "docente_activo = true AND carrera_activo = true;";
+                + "docente_activo = true AND \n"
+                + "pl.id_prd_lectivo = c.id_prd_lectivo AND \n"
+                + "prd_lectivo_activo = true;";
         return consultarCursos(sql);
     }
 
@@ -93,13 +95,16 @@ public class CursoBD extends CursoMD {
         String sql = "SELECT id_curso, materia_nombre, \n"
                 + "persona_primer_nombre, persona_primer_apellido, \n"
                 + "curso_nombre, curso_capacidad, curso_ciclo, \n"
-                + "c.id_prd_lectivo, c.id_materia, c.id_docente\n"
+                + "c.id_prd_lectivo, c.id_materia, c.id_docente, prd_lectivo_nombre\n"
                 + "FROM public.\"Cursos\" c, public.\"Materias\" m, \n"
-                + "public.\"Docentes\" d, public.\"Personas\" p\n"
+                + "public.\"Docentes\" d, public.\"Personas\" p, \n"
+                + "public.\"PeriodoLectivo\" pl \n"
                 + "WHERE m.id_materia = c.id_materia AND \n"
                 + "d.id_docente = c.id_docente AND \n"
                 + "p.id_persona = d.id_persona \n"
-                + "AND id_prd_lectivo = " + idPrdLectivo + ";";
+                + "AND C.id_prd_lectivo = " + idPrdLectivo + " AND \n"
+                + "pl.id_prd_lectivo = c.id_prd_lectivo AND \n"
+                + "prd_lectivo_activo = true;";
         return consultarCursos(sql);
     }
 
@@ -107,13 +112,16 @@ public class CursoBD extends CursoMD {
         String sql = "SELECT id_curso, materia_nombre, \n"
                 + "persona_primer_nombre, persona_primer_apellido, \n"
                 + "curso_nombre, curso_capacidad, curso_ciclo, \n"
-                + "c.id_prd_lectivo, c.id_materia, c.id_docente\n"
+                + "c.id_prd_lectivo, c.id_materia, c.id_docente, \n"
+                + "prd_lectivo_nombre \n"
                 + "FROM public.\"Cursos\" c, public.\"Materias\" m, \n"
-                + "public.\"Docentes\" d, public.\"Personas\" p\n"
+                + "public.\"Docentes\" d, public.\"Personas\" p, \n"
+                + "public.\"PeriodoLectivo\" pl\n"
                 + "WHERE m.id_materia = c.id_materia AND \n"
                 + "d.id_docente = c.id_docente AND \n"
                 + "p.id_persona = d.id_persona \n"
-                + "AND curso_nombre = '" + nombre + "';";
+                + "AND curso_nombre = '" + nombre + "' AND \n"
+                + "pl.id_prd_lectivo = c.id_prd_lectivo;";
         return consultarCursos(sql);
     }
 
@@ -121,13 +129,18 @@ public class CursoBD extends CursoMD {
         String sql = "SELECT id_curso, materia_nombre, \n"
                 + "persona_primer_nombre, persona_primer_apellido, \n"
                 + "curso_nombre, curso_capacidad, curso_ciclo, \n"
-                + "c.id_prd_lectivo, c.id_materia, c.id_docente\n"
+                + "c.id_prd_lectivo, c.id_materia, c.id_docente, \n"
+                + "prd_lectivo_nombre \n"
                 + "FROM public.\"Cursos\" c, public.\"Materias\" m, \n"
-                + "public.\"Docentes\" d, public.\"Personas\" p\n"
+                + "public.\"Docentes\" d, public.\"Personas\" p, \n"
+                + "public.\"PeriodoLectivo\" pl \n"
                 + "WHERE m.id_materia = c.id_materia AND \n"
                 + "d.id_docente = c.id_docente AND \n"
                 + "p.id_persona = d.id_persona \n"
-                + "AND curso_nombre = '" + nombre + "' AND id_prd_lectivo = " + idPrdLectivo + ";";
+                + "AND curso_nombre = '" + nombre + "' AND \n"
+                + "c.id_prd_lectivo = " + idPrdLectivo + " \n"
+                + "pl.id_prd_lectivo = c.id_prd_lectivo AND \n"
+                + "prd_lectivo_activo = true;";
         return consultarCursos(sql);
     }
 
@@ -135,17 +148,20 @@ public class CursoBD extends CursoMD {
         String sql = "SELECT id_curso, materia_nombre, \n"
                 + "persona_primer_nombre, persona_primer_apellido, \n"
                 + "curso_nombre, curso_capacidad, curso_ciclo, \n"
-                + "c.id_prd_lectivo, c.id_materia, c.id_docente\n"
+                + "c.id_prd_lectivo, c.id_materia, c.id_docente, \n"
+                + "prd_lectivo_nombre \n"
                 + "FROM public.\"Cursos\" c, public.\"Materias\" m, \n"
-                + "public.\"Docentes\" d, public.\"Personas\" p, public.\"Carreras\" cr\n"
+                + "public.\"Docentes\" d, public.\"Personas\" p, \n"
+                + "public.\"PeriodoLectivo\" pl \n"
                 + "WHERE m.id_materia = c.id_materia AND \n"
-                + "cr.id_carrera = m.id_carrera AND\n"
                 + "d.id_docente = c.id_docente AND \n"
                 + "p.id_persona = d.id_persona AND \n"
+                + "pl.id_prd_lectivo = c.id_prd_lectivo AND\n"
                 + "(materia_nombre ILIKE '%" + aguja + "%' OR \n"
-                + "persona_primer_nombre || ' ' || persona_primer_apellido ILIKE '%" + aguja + "%'\n"
+                + "persona_primer_nombre || ' ' || persona_segundo_nombre || ' ' || "
+                + "persona_primer_apellido || ' ' || persona_segundo_apellido ILIKE '%" + aguja + "%'\n"
                 + "OR persona_identificacion ILIKE '%" + aguja + "%') AND \n"
-                + "persona_activa = true AND docente_activo = true AND carrera_activo = true;";
+                + "persona_activa = true AND docente_activo = true;";
         return consultarCursos(sql);
     }
 
@@ -326,6 +342,7 @@ public class CursoBD extends CursoMD {
             c.setId_materia(m);
             PeriodoLectivoMD p = new PeriodoLectivoMD();
             p.setId_PerioLectivo(rs.getInt("id_prd_lectivo"));
+            p.setNombre_PerLectivo(rs.getString("prd_lectivo_nombre"));
             c.setId_prd_lectivo(p);
             DocenteMD d = new DocenteMD();
             d.setIdDocente(rs.getInt("id_docente"));
