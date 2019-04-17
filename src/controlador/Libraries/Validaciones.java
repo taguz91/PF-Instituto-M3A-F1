6a92f0c6b9e322455ccba59a98018bab7d2a6ab0
MrRainx <diegocondo10@github.com>
2019-04-16 19:46:30 -0500

    trabajando en los tipos de notas

--- src/controlador/Libraries/Validaciones.java
+++ src/controlador/Libraries/Validaciones.java
@@ -1,17 +1,26 @@
 package controlador.Libraries;
 
+import java.awt.event.KeyAdapter;
+import javax.swing.JComponent;
+
 /**
  *
  * @author MrRainx
  */
 public class Validaciones {
-    
+
     public static boolean isInt(String Number) {
         return Number.matches("^[0-9]");
     }
 
     public static boolean isDecimal(String Number) {
         return Number.matches("^[0-9]*+[.]{0,1}+[0-9]*");
+    }
+
+    public static void validardarNumeroDecimalEnTxtField(JComponent component) {
+        component.addKeyListener(new KeyAdapter() {
+            
+        });
     }
 
 }
--- src/controlador/periodoLectivoNotas/FrmTipoNotaCTR.java
+++ src/controlador/periodoLectivoNotas/FrmTipoNotaCTR.java
@@ -3,10 +3,11 @@ package controlador.periodoLectivoNotas;
 import controlador.Libraries.Middlewares;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.beans.PropertyVetoException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
+import java.util.List;
+import modelo.carrera.CarreraBD;
+import modelo.carrera.CarreraMD;
 import modelo.tipoDeNota.TipoDeNotaBD;
 import vista.periodoLectivoNotas.FrmTipoNota;
 import vista.principal.VtnPrincipal;
@@ -22,6 +23,8 @@ public class FrmTipoNotaCTR {
     private TipoDeNotaBD modelo;
     //Ventana Padre
     private VtnTipoNotasCTR vtnPadre;
+    //listas
+    private List<CarreraMD> listaCarreras;
     //(Agregar o Editar)
     private String Funcion;
 
@@ -37,12 +40,45 @@ public class FrmTipoNotaCTR {
 
     //INITS
     public void Init() {
-
         new Thread(() -> {
-            
+            listaCarreras = CarreraBD.selectIdNombreAll();
+            cargarComboCarreras();
+            InitEventos();
         }).start();
+        try {
+            Middlewares.centerFrame(vista, desktop.getDpnlPrincipal());
+            desktop.getDpnlPrincipal().add(vista);
+            vista.setSelected(true);
+            vista.show();
+        } catch (PropertyVetoException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void InitEventos() {
+        vista.getBtnCancelar().addActionListener(e -> btnCancelar(e));
+        vista.getTxtNotaMin().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                txtNotaMinOnKeyReleased(e);
+            }
+        });
     }
 
     //METODOS DE APOYO
+    private void cargarComboCarreras() {
+
+        listaCarreras.stream().forEach(obj -> {
+            vista.getCmbCarrera().addItem(obj.getNombre());
+        });
+    }
+
     //EVENTOS
+    private void btnCancelar(ActionEvent e) {
+        vista.dispose();
+    }
+
+    private void txtNotaMinOnKeyReleased(KeyEvent e) {
+        
+    }
 }
