50c5c64ccaab5f0231aae758345e97a268af13d5
Gustavo <johnnygar98@hotmail.com>
2019-04-17 23:27:42 -0500

    CRUD de sesion de clases medio terminado

--- BaseDatos/Consultas/G23.sql
+++ BaseDatos/Consultas/G23.sql
@@ -32,7 +32,7 @@ SELECT COUNT(*) FROM (public."Carreras" c JOIN public."AlumnosCarrera" a
 					  USING(id_carrera)) JOIN public."MallaAlumno" m USING(id_almn_carrera)
 							WHERE c.id_carrera = 2 AND m.malla_almn_estado LIKE 'M';
 
---Consultamos todos los alumnos de una malla por carrera 
+--Consultamos todos los alumnos de una malla por carrera
 SELECT id_malla_alumno, id_materia, ma.id_almn_carrera,
 malla_almn_ciclo, malla_almn_num_matricula,
 malla_almn_nota1, malla_almn_nota2,
@@ -41,3 +41,12 @@ FROM public."MallaAlumno" ma, public."AlumnosCarrera" ac
 WHERE ac.id_carrera = 2
 AND ma.id_almn_carrera = ac.id_almn_carrera
 AND malla_almn_estado = 'M';
+
+--Consultamos los horarios con el periodo lectivo
+
+	SELECT id_sesion, sc.id_curso, dia_sesion, hora_inicio_sesion, hora_fin_sesion,
+curso_nombre, id_prd_lectivo, id_jornada, materia_nombre
+	FROM public."SesionClase" sc, public."Cursos" c,
+	public."Materias" m
+	WHERE c.id_curso = sc.id_curso AND
+	m.id_materia = c.id_materia;
---
+++ src/modelo/curso/SesionClaseMD.java
@@ -0,0 +1,60 @@
+package modelo.curso;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Johnny
+ */
+public class SesionClaseMD {
+    
+    private int id; 
+    private int dia; 
+    private CursoMD curso;
+    private LocalTime horaIni, horaFin;
+
+    public SesionClaseMD() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public CursoMD getCurso() {
+        return curso;
+    }
+
+    public void setCurso(CursoMD curso) {
+        this.curso = curso;
+    }
+
+    public LocalTime getHoraIni() {
+        return horaIni;
+    }
+
+    public void setHoraIni(LocalTime horaIni) {
+        this.horaIni = horaIni;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+    
+    
+}
