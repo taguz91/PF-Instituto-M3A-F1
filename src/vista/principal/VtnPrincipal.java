0f28eb9a7b1a7b50a7864da6b1428847835db0a9
bycesar <ces.pes.99@gmail.com>
2019-04-18 16:53:59 -0500

    Update VtnPrincipal.java

--- src/vista/principal/VtnPrincipal.java
+++ src/vista/principal/VtnPrincipal.java
@@ -335,9 +335,6 @@ public class VtnPrincipal extends javax.swing.JFrame {
         this.mnCtMiPerfil = mnCtMiPerfil;
     }
     
-    
-    
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
