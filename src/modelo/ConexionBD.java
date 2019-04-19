ee1051faf895bc6ed77df565530a3041a3b97055
Gustavo <johnnygar98@hotmail.com>
2019-04-19 12:32:45 -0500

    Vamos a configurar un poll de conexiones

--- src/modelo/ConexionBD.java
+++ src/modelo/ConexionBD.java
@@ -61,7 +61,6 @@ public class ConexionBD {
     }
 
     public Connection getCon() {
-<<<<<<< HEAD
         
         // Comprobar conexion
         
@@ -70,7 +69,6 @@ public class ConexionBD {
 //            con = ResourceManager.getConnection();
 //        } catch (SQLException ex) {
 //            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
-=======
 //        try {
 //            if (!con.isClosed()) {
 //                con.close();
@@ -84,7 +82,6 @@ public class ConexionBD {
 //        } catch (SQLException ex) {
 //            System.out.println("Error al comprobar la conexion");
 //            System.out.println(ex.getMessage());
->>>>>>> e599953f895825afbb823848abc7c1fe028a1393
 //        }
 
         return con;
