25826dd0de11229330757b2099aebdbd027c139c
Joanber <47487527+Joanber@users.noreply.github.com>
2019-04-21 22:46:47 -0500

    plan_clases_silabo

--- src/controlador/silabo/Controlador_plan_clases.java
+++ src/controlador/silabo/Controlador_plan_clases.java
@@ -1,7 +1,13 @@
 
 package controlador.silabo;
 
+import java.util.List;
 import modelo.ConexionBD;
+import modelo.curso.CursoMD;
+import modelo.silabo.CursoMDS;
+import modelo.silabo.CursosBDS;
+import modelo.silabo.SilaboMD;
+import modelo.unidadSilabo.UnidadSilaboMD;
 import modelo.usuario.UsuarioBD;
 import vista.principal.VtnPrincipal;
 import vista.silabos.frmPlanClase;
@@ -12,7 +18,17 @@ public class Controlador_plan_clases {
     private ConexionBD conexion;
     private final VtnPrincipal vtnPrincipal;
     private frmPlanClase fPlanClase;
-    public Controlador_plan_clases(UsuarioBD usuario, VtnPrincipal vtnPrincipal) {
+    private SilaboMD silabo;
+    private CursoMD curso;
+    private UnidadSilaboMD unidadsilabo;
+    private List<SilaboMD> lista_silabo;
+    private List<CursoMDS> lista_curso;
+    private List<UnidadSilaboMD> lista_unidadsilabo;
+    
+    public Controlador_plan_clases(SilaboMD silabo,CursoMD curso,UnidadSilaboMD unidadsilabo,UsuarioBD usuario, VtnPrincipal vtnPrincipal) {
+        this.silabo=silabo;
+        this.curso=curso;
+        this.unidadsilabo=unidadsilabo;
         this.usuario = usuario;
         this.vtnPrincipal = vtnPrincipal;
         this.conexion = new ConexionBD();
@@ -30,11 +46,31 @@ public class Controlador_plan_clases {
              ControladorConfiguracion_plan_clases ccpc=new ControladorConfiguracion_plan_clases(usuario, vtnPrincipal);
              ccpc.iniciarControlaador();
          });
-           
-           
+           IniciaPlanClase(silabo, curso, unidadsilabo);
+        
      }
     
-    
+    private void IniciaPlanClase(SilaboMD silabo,CursoMD curso,UnidadSilaboMD unidadsilabo){
+      lista_curso=CursosBDS.ConsultarCursoCarreraDocente(conexion, curso.getId_curso());
+        cargarCamposCursoCarreraDocente(lista_curso);
+    }
+    public  void cargarCamposCursoCarreraDocente(List<CursoMDS> lista){
+        for (CursoMDS cursoMDS : lista) {
+        fPlanClase.getTxtCarrera().setText(cursoMDS.getId_carrera().getNombre());
+        fPlanClase.getTxtCarrera().setEnabled(false);
+        fPlanClase.getTxtDocente().setText(cursoMDS.getId_persona().getPrimerNombre()+" "+cursoMDS.getId_persona().getPrimerApellido());
+        fPlanClase.getTxtDocente().setEnabled(false);
+        fPlanClase.getTxtAsignatura().setText(cursoMDS.getId_materia().getNombre());
+        fPlanClase.getTxtAsignatura().setEnabled(false);
+        fPlanClase.getTxtCod_Asignatura().setText(cursoMDS.getId_materia().getCodigo());
+        fPlanClase.getTxtCod_Asignatura().setEnabled(false);
+        fPlanClase.getTxtCicloParalelo().setText(cursoMDS.getCurso_nombre());
+        fPlanClase.getTxtCicloParalelo().setEnabled(false);
+        
+        
+            
+        }
+    }
     
     
      
