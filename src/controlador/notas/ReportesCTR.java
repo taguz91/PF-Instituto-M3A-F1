7f966ac9d57cd3c791061868fc42688bd538cf44
lusho31 <47617208+lusho31@users.noreply.github.com>
2019-04-24 01:31:52 -0500

    .

--- src/controlador/notas/ReportesCTR.java
+++ src/controlador/notas/ReportesCTR.java
@@ -78,13 +78,13 @@ public class ReportesCTR {
         String ciclo = vista.getCmbCiclo().getSelectedItem().toString();
         String materia = vista.getCmbAsignatura().getSelectedItem().toString();
 
-        String path = "./src/vista/notas/reportesPresencial/ReporteNotasEntre80y90.jasper";
+        String path = "./src/vista/notas/reportesPresencial/ReporteEntre80y90_Presencial.jasper";
 
         Map parametros = new HashMap();
 
         parametros.put("id_docente", idDocente);
         parametros.put("prd_lectivo_nombre", String.valueOf(nombrePeriodo));
-        parametros.put("curso_ciclo", new Integer(ciclo));
+        parametros.put("curso_nombre", ciclo);
         parametros.put("materia_nombre", materia);
 
         Middlewares.generarReporte(path, "Reporte Entre 80 y 90", parametros);
