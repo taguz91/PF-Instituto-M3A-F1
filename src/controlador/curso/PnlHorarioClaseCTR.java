1b24a35d6ad3c50de3456032df55a427378e1bd0
DannesM <dario45678910@gmail.com>
2019-04-24 14:07:38 -0500

    Cambio en horas

--- src/controlador/curso/PnlHorarioClaseCTR.java
+++ src/controlador/curso/PnlHorarioClaseCTR.java
@@ -34,7 +34,7 @@ public class PnlHorarioClaseCTR {
         "<html>10:00<br>11:00</html>",
         "<html>11:00<br>12:00</html>",
         "<html>12:00<br>13:00</html>"};
-    private final String[] hv = {"14:00", "15:00", "16:00", "17:00", "18:00", "19:00","20:00"};
+    private final String[] hv = {"14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"};
     private final String[] hvc = {
         "<html>14:00<br>15:00</html>",
         "<html>15:00<br>16:00</html>",
@@ -42,10 +42,10 @@ public class PnlHorarioClaseCTR {
         "<html>17:00<br>18:00</html>",
         "<html>18:00<br>19:00</html>",
         "<html>19:00<br>20:00</html>"};
-    private final String[] hn = {"8:00","9:00","10:00", "11:00", "12:00", "13:00","17:00", "18:00", "19:00", "20:00", "21:00", "22:00"};
+    private final String[] hn = {"08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"};
     private final String[] hnc = {
-        "<html>8:00<br>9:00</html>",
-        "<html>9:00<br>10:00</html>",
+        "<html>08:00<br>09:00</html>",
+        "<html>09:00<br>10:00</html>",
         "<html>10:00<br>11:00</html>",
         "<html>11:00<br>12:00</html>",
         "<html>12:00<br>13:00</html>",
@@ -53,7 +53,8 @@ public class PnlHorarioClaseCTR {
         "<html>18:00<br>19:00</html>",
         "<html>19:00<br>20:00</html>",
         "<html>20:00<br>21:00</html>",
-        "<html>21:00<br>22:00</html>"};
+        "<html>21:00<br>22:00</html>",
+        "<html>22:00<br>23:00</html>"};
     private String[] hSelec, jSelec, tSelec;
     private int posI, posF, posFila, posColum;
 
@@ -147,7 +148,7 @@ public class PnlHorarioClaseCTR {
         }
 
         for (int i = 0; i < hSelec.length; i++) {
-            
+
             //System.out.print("Hora: "+hSelec[i]+" : "+tranformar(s.getHoraFin())+ " Igual: "+hSelec[i].equals(tranformar(s.getHoraFin())));
             if (hSelec[i].equals(tranformar(s.getHoraFin()))) {
                 posF = i;
@@ -157,7 +158,7 @@ public class PnlHorarioClaseCTR {
         }
 
         for (int i = posI; i < posF; i++) {
-            mdTbl.setValueAt(s.getId()+"%Clase", i, dia);
+            mdTbl.setValueAt(s.getId() + "%Clase", i, dia);
         }
 
     }
@@ -214,7 +215,7 @@ public class PnlHorarioClaseCTR {
         SesionClaseMD s = null;
         if (posFila >= 0 && posColum > 0) {
             if (pnl.getTblHorario().getValueAt(posFila, posColum) != null) {
-                 
+
             }
         }
         return s;
@@ -227,7 +228,7 @@ public class PnlHorarioClaseCTR {
     public String[] getjSelec() {
         return jSelec;
     }
-    
+
     public String[] gettSelec() {
         return tSelec;
     }
