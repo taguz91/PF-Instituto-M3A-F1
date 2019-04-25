9a318ecd04a2daef9f968879361a82f52ca2c2e7
Ubuntu Machine - Insti L1 <johnnygar98@hotmail.com>
2019-04-25 18:33:55 -0500

    Consulta para notas aun no subidas

--- BaseDatos/Consultas/Matriculas.sql
+++ BaseDatos/Consultas/Matriculas.sql
@@ -15,3 +15,24 @@ WHERE id_curso IN(
   FROM public."Cursos"
   WHERE id_prd_lectivo = 8
 )
+
+
+--NOtas aun no ingresadas
+SELECT * FROM public."AlumnoCurso"
+WHERE id_curso IN(
+  SELECT id_curso
+  FROM public."Cursos"
+  WHERE id_prd_lectivo = 4
+)AND almn_curso_nota_final 0;
+
+SELECT pa.persona_primer_nombre || '' || pa.persona_primer_apellido AS alumnos,
+almn_curso_nota_final, curso_nombre,
+pd.persona_primer_nombre || '' || pd.persona_primer_apellido AS docentes
+FROM public."Alumnos" a, public."Personas" pa, public."Docentes" d,
+public."Personas" pd, public."Cursos" c, public."AlumnosCurso" ac
+WHERE ac.id_curso = c.id_curso AND
+c.id_prd_lectivo IN (4, 8) AND
+a.id_alum = ac.id_alumno AND
+pa.id_persona = a.id_persona AND
+d.id_docente = c.id_docente AND
+pd.id_persona = d.id_persona;
