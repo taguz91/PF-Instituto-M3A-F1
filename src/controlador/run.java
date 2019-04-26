928199bed749ae15d69d77749873d1f7f627afa2
Gustavo <johnnygar98@hotmail.com>
2019-04-25 23:45:00 -0500

    Configurada para que entre por defecto con windows o nimbus, siepre con provilegio de windows porque ahi no tenemos errores graficos

--- src/controlador/run.java
+++ src/controlador/run.java
@@ -11,23 +11,43 @@ import vista.Login;
  * @author Johnny
  */
 public class run {
-
+    
     public static void main(String[] args) {
-        try {
-            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(run.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        //No borrar !!!
+//        try {
+//            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+//        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(run.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+
+        if (!iniciaEstilo("Windows")) {
+            iniciaEstilo("Nimbus"); 
         }
 
 //        SplashCTR ctrSplash = new SplashCTR();
 //        ctrSplash.iniciar();
         java.awt.EventQueue.invokeLater(() -> {
-
+            
             LoginCTR login = new LoginCTR(new Login(), new UsuarioBD());
             login.Init();
-
+            
         });
-
     }
-
+    
+    public static boolean iniciaEstilo(String estilo) {
+        boolean encontrado = false;        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if (estilo.equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    encontrado = true;
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            System.out.println("No enconramos ninugn LOOK AND FIELD " + ex.getMessage());
+        }
+        return true;
+    }
+    
 }
