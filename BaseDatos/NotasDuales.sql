c15e7094ce9f90bb28cb980b91e7dc65b6825cff
MrRainx <diegocondo10@yahoo.es>
2019-05-02 11:21:19 -0500

    trabajando en las notas Duales. SCRIPT

--- BaseDatos/NotasDuales.sql
+++ BaseDatos/NotasDuales.sql
@@ -1,11 +1,12 @@
 CREATE TABLE "PTI"
 (
     id_pti serial NOT NULL PRIMARY KEY,
-    pti_nota NUMERIC(6,2) DEFAULT 0
+    pti_nota NUMERIC(6,2) DEFAULT 0,
+    pti_estado VARCHAR(30) DEFAULT "REPROBADO"
 );
 
 
-CREATE TABLE "ALMN_PTI"
+CREATE TABLE "DetallePTI"
 (
     id_almn_pti serial NOT NULL PRIMARY KEY,
     id_almn_curso INTEGER,
@@ -13,13 +14,38 @@ CREATE TABLE "ALMN_PTI"
 );
 
 
-ALTER TABLE "ALMN_PTI" ADD CONSTRAINT "PTI__ALMN_PTI_fk"
+
+
+CREATE TABLE "DetalleFasePractica"(
+    id_detalle_f_prac serial NOT NULL PRIMARY KEY,
+    id_almn_curso INTEGER,
+    id_fase_practica INTEGER
+
+);
+
+CREATE TABLE "FasePractica"(
+    id_fase_practica serial NOT NULL PRIMARY KEY,
+    fase_prac_nota_final INTEGER DEFAULT 0,
+    fase_pac_estado VARCHAR(20) DEFAULT "REPROBADO"
+);
+
+
+CREATE TABLE "NotasPracticas"(
+    id_nota_prac serial NOT NULL PRIMARY KEY,
+    nota_prac_valor NUMERIC(6,2) DEFAULT 0
+    id_fase_practica INTEGER,
+    id_tipo_nota INTEGER
+);
+
+
+
+ALTER TABLE "detallePTI" ADD CONSTRAINT "detallePTI__PTI_fk"
 FOREIGN KEY (id_pti) REFERENCES "PTI"(id_pti) ON
 DELETE CASCADE ON
 UPDATE CASCADE;
 
 
-ALTER TABLE "ALMN_PTI" ADD CONSTRAINT "ALMN_PTI__AlumnoCurso_fk"
+ALTER TABLE "detallePTI" ADD CONSTRAINT "detallePTI__AlumnoCurso_fk"
 FOREIGN KEY (id_almn_curso) REFERENCES "AlumnoCurso"(id_almn_curso) ON
 DELETE CASCADE ON
 UPDATE CASCADE;
