4f000e23076abb96320863b3495a4288f230500a
AndresUV7 <andres.ullauri18@gmail.com>
2019-05-06 10:52:01 -0500

    Implementacion Segunda Version Silabo Permiso Anteriores

--- BaseDatos/NotasDuales.sql
+++ BaseDatos/NotasDuales.sql
@@ -1,11 +1,12 @@
 CREATE TABLE "PTI"
 (
     id_pti serial NOT NULL PRIMARY KEY,
-    pti_nota NUMERIC(6,2) DEFAULT 0
+    pti_nota NUMERIC(6,2) DEFAULT 0,
+    pti_estado VARCHAR(30) DEFAULT 'REPROBADO'
 );
 
 
-CREATE TABLE "ALMN_PTI"
+CREATE TABLE "DetallePTI"
 (
     id_almn_pti serial NOT NULL PRIMARY KEY,
     id_almn_curso INTEGER,
@@ -13,13 +14,60 @@ CREATE TABLE "ALMN_PTI"
 );
 
 
-ALTER TABLE "ALMN_PTI" ADD CONSTRAINT "PTI__ALMN_PTI_fk"
+
+
+CREATE TABLE "DetalleFasePractica"(
+    id_detalle_f_prac serial NOT NULL PRIMARY KEY,
+    id_almn_curso INTEGER,
+    id_fase_practica INTEGER
+
+);
+
+CREATE TABLE "FasePractica"(
+    id_fase_practica serial NOT NULL PRIMARY KEY,
+    fase_prac_nota_final INTEGER DEFAULT 0,
+    fase_pac_estado VARCHAR(20) DEFAULT 'REPROBADO'
+);
+
+
+CREATE TABLE "NotasPracticas"(
+    id_nota_prac serial NOT NULL PRIMARY KEY,
+    nota_prac_valor NUMERIC(6,2) DEFAULT 0,
+    id_fase_practica INTEGER,
+    id_tipo_nota INTEGER
+);
+
+
+
+ALTER TABLE "DetallePTI" ADD CONSTRAINT "detallePTI__PTI_fk"
 FOREIGN KEY (id_pti) REFERENCES "PTI"(id_pti) ON
 DELETE CASCADE ON
 UPDATE CASCADE;
 
 
-ALTER TABLE "ALMN_PTI" ADD CONSTRAINT "ALMN_PTI__AlumnoCurso_fk"
+ALTER TABLE "DetallePTI" ADD CONSTRAINT "detallePTI__AlumnoCurso_fk"
 FOREIGN KEY (id_almn_curso) REFERENCES "AlumnoCurso"(id_almn_curso) ON
 DELETE CASCADE ON
 UPDATE CASCADE;
+
+ALTER TABLE "DetalleFasePractica" ADD CONSTRAINT "detalle__fase_pract_fk"
+    FOREIGN KEY (id_fase_practica) REFERENCES "FasePractica"(id_fase_practica) 
+        ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE "DetalleFasePractica" ADD CONSTRAINT "detalle__alumno_curso__fk"
+    FOREIGN KEY(id_almn_curso) REFERENCES "AlumnoCurso" (id_almn_curso)
+        ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "NotasPracticas" ADD CONSTRAINT "fase_practica__notas_practicas__fk"
+    FOREIGN KEY(id_fase_practica) REFERENCES "FasePractica"(id_fase_practica)
+        ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "NotasPracticas" ADD CONSTRAINT "tipo_nota__notas_practicas__fk"
+    FOREIGN KEY (id_tipo_nota) REFERENCES "TipoDeNota"(id_tipo_nota)
+        ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+
+
+0703630715=CARRANZA OCHOA ROBERTO  NATANIEL\t0995710017\trncarranza76@gmail.com\tTIEMPO COMPLETO
--- src/controlador/silabo/ControladorCRUD.java
+++ src/controlador/silabo/ControladorCRUD.java
@@ -68,11 +68,11 @@ public class ControladorCRUD {
 
         crud.setLocation((principal.getDpnlPrincipal().getSize().width - crud.getSize().width) / 2,
                 (principal.getDpnlPrincipal().getSize().height - crud.getSize().height) / 2);
-        
+
         opcionesImpresion(false);
         // Boton NUEVO Silabo
         crud.getBtnNuevo().addActionListener((ActionEvent ae) -> {
-            
+
             crud.dispose();
 
             ControladorSilaboC csc = new ControladorSilaboC(principal, usuario, conexion);
@@ -82,48 +82,50 @@ public class ControladorCRUD {
 
         // Boton EDITAR Silabo
         crud.getBtnEditar().addActionListener((ActionEvent ae) -> {
-         int row =crud.getTblSilabos().getSelectedRow();
-            if (row!=-1) {
-                
-            crud.dispose();
+            int row = crud.getTblSilabos().getSelectedRow();
+            if (row != -1) {
 
-            ControladorSilaboU csu = new ControladorSilaboU(seleccionarSilabo(), principal, conexion);
+                if (seleccionarSilabo() != null) {
+                    crud.dispose();
 
-            csu.iniciarControlador();
-            
-            }else{
-             JOptionPane.showMessageDialog(null, "Seleccione un silabo", "Aviso", JOptionPane.ERROR_MESSAGE);
+                    ControladorSilaboU csu = new ControladorSilaboU(seleccionarSilabo(), principal, conexion);
+
+                    csu.iniciarControlador();
+                } else {
+                    JOptionPane.showMessageDialog(null, "No puede editar silabos correspondientes a un periodo anterior", "Aviso", JOptionPane.WARNING_MESSAGE);
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Seleccione un silabo", "Aviso", JOptionPane.ERROR_MESSAGE);
             }
         });
 
         // Boton ELIMINAR Silabo
         crud.getBtnEliminar().addActionListener((ActionEvent ae) -> {
-        int row =crud.getTblSilabos().getSelectedRow();
-            if (row!=-1) {
-                
-            
-            eliminarSilabo();
-            cargarSilabosDocente();
-            
-            }else{
-            JOptionPane.showMessageDialog(null, "Seleccione un silabo", "Aviso", JOptionPane.ERROR_MESSAGE);
+            int row = crud.getTblSilabos().getSelectedRow();
+            if (row != -1) {
+
+                eliminarSilabo();
+                cargarSilabosDocente();
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Seleccione un silabo", "Aviso", JOptionPane.ERROR_MESSAGE);
             }
 
         });
 
         crud.getBtnImprimir().addActionListener((ActionEvent ae) -> {
-            
-            
+
             //VALIDA QUE SELECCIONE UN SILABO E IMPRIMA
-            int row =crud.getTblSilabos().getSelectedRow();
-            
-            if (row!=-1) {
-                
-            opcionesImpresion(true);
+            int row = crud.getTblSilabos().getSelectedRow();
 
-            ControladorSilaboR csr = new ControladorSilaboR(crud, seleccionarSilabo(), conexion, principal);
+            if (row != -1) {
 
-            csr.iniciarControlador();
+                opcionesImpresion(true);
+
+                ControladorSilaboR csr = new ControladorSilaboR(crud, seleccionarSilabo(), conexion, principal);
+
+                csr.iniciarControlador();
             } else {
                 JOptionPane.showMessageDialog(null, "Seleccione un silabo", "Aviso", JOptionPane.ERROR_MESSAGE);
             }
@@ -184,7 +186,6 @@ public class ControladorCRUD {
                 }
 
                 modeloTabla.addRow(new Object[]{
-                    
                     smd.getIdMateria().getNombre(),
                     smd.getIdPeriodoLectivo().getFecha_Inicio() + " / " + smd.getIdPeriodoLectivo().getFecha_Fin(),
                     estado,
@@ -195,10 +196,10 @@ public class ControladorCRUD {
             crud.getTblSilabos().setModel(modeloTabla);
 
         } catch (Exception e) {
-            
-               JOptionPane.showMessageDialog(null, "Usted no tiene carreras asignadas en el presente periodo", "Aviso", JOptionPane.ERROR_MESSAGE);
-            crud.dispose();    
-            
+
+            JOptionPane.showMessageDialog(null, "Usted no tiene carreras asignadas en el presente periodo", "Aviso", JOptionPane.ERROR_MESSAGE);
+            crud.dispose();
+
         }
 
     }
@@ -206,15 +207,20 @@ public class ControladorCRUD {
     public SilaboMD seleccionarSilabo() {
 
         int seleccion = crud.getTblSilabos().getSelectedRow();
-        
+
         Optional<SilaboMD> silaboSeleccionado = silabosDocente.stream().
-                filter(s -> s.getIdSilabo()==Integer.parseInt(crud.getTblSilabos().getValueAt(seleccion, 3).toString())).
+                filter(s -> s.getIdSilabo() == Integer.parseInt(crud.getTblSilabos().getValueAt(seleccion, 3).toString())).
                 findFirst();
-        
-        /*List<PeriodoLectivoMD>periodosCarrera = PeriodoLectivoBDS.consultar(conexion, carreraSeleccionada.get().getId());
-        periodosCarrera.stream().findFirst().get();
-        
-        if (silaboSeleccionado.get().getIdPeriodoLectivo())*/
+
+        CarreraMD carrera = CarrerasBDS.consultar(conexion, usuario.getUsername()).stream().
+                filter(c -> c.getNombre().equals(crud.getCmbCarrera().getSelectedItem().toString())).findFirst().get();
+
+        List<PeriodoLectivoMD> periodosCarrera = PeriodoLectivoBDS.consultar(conexion, carrera.getId());
+        PeriodoLectivoMD ultimo = periodosCarrera.stream().findFirst().get();
+
+        if (silaboSeleccionado.get().getIdPeriodoLectivo().getId_PerioLectivo() != ultimo.getId_PerioLectivo()) {
+            return null;
+        }
 
         return silaboSeleccionado.get();
     }
@@ -230,15 +236,17 @@ public class ControladorCRUD {
 
     public void eliminarSilabo() {
 
-        int reply = JOptionPane.showConfirmDialog(null, "¿Está seguro que desea eliminar este silabo?", "Eliminar", JOptionPane.YES_NO_OPTION);
-        if (reply == JOptionPane.YES_OPTION) {
-            new SilaboBD(conexion).eliminar(seleccionarSilabo());
-            JOptionPane.showMessageDialog(null, "Silabo eliminado correctamente");
+        if (seleccionarSilabo() != null) {
+            int reply = JOptionPane.showConfirmDialog(null, "¿Está seguro que desea eliminar este silabo?", "Eliminar", JOptionPane.YES_NO_OPTION);
+            if (reply == JOptionPane.YES_OPTION) {
+                new SilaboBD(conexion).eliminar(seleccionarSilabo());
+                JOptionPane.showMessageDialog(null, "Silabo eliminado correctamente");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No puede eliminar silabos correspondientes a un periodo anterior", "Aviso", JOptionPane.WARNING_MESSAGE);
+               
         }
 
     }
-    
-    
-    
 
 }
