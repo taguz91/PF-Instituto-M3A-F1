afd55270e7fcd183223dd8dc7b5a6aa3526d025b
keke1917 <locmarce17@hotmail.com>
2019-05-07 09:48:20 -0500

    Cambio checkbox por tabla

--- src/vista/periodoLectivoNotas/FrmTipoNota.java
+++ src/vista/periodoLectivoNotas/FrmTipoNota.java
@@ -39,13 +39,7 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
         this.btnGuardar = btnGuardar;
     }
 
-    public JComboBox<String> getCmbTipoDeNota() {
-        return cmbTipoDeNota;
-    }
-
-    public void setCmbTipoDeNota(JComboBox<String> cmbTipoDeNota) {
-        this.cmbTipoDeNota = cmbTipoDeNota;
-    }
+ 
 
     public JComboBox<String> getCmbPeriodoLectivo() {
         return cmdPeriodoLectivo;
@@ -55,21 +49,7 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
         this.cmdPeriodoLectivo = cmdPeriodoLectivo;
     }
 
-    public JTextField getTxtNotaMax() {
-        return txtNotaMax;
-    }
-
-    public void setTxtNotaMax(JTextField txtNotaMax) {
-        this.txtNotaMax = txtNotaMax;
-    }
-
-    public JTextField getTxtNotaMin() {
-        return txtNotaMin;
-    }
-
-    public void setTxtNotaMin(JTextField txtNotaMin) {
-        this.txtNotaMin = txtNotaMin;
-    }
+   
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -78,9 +58,12 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
         btnCancelar = new javax.swing.JButton();
         btnGuardar = new javax.swing.JButton();
         jSeparator1 = new javax.swing.JSeparator();
-        jSeparator2 = new javax.swing.JSeparator();
         lblCarrera = new javax.swing.JLabel();
         cmdPeriodoLectivo = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblTipoNota = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        lblNombreCarrera = new javax.swing.JLabel();
 
         setClosable(true);
         setIconifiable(true);
@@ -94,23 +77,53 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
         lblCarrera.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lblCarrera.setText("Periodo Lectivo:");
 
+        tblTipoNota.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Minimo", "Maximo"
+            }
+        ));
+        jScrollPane1.setViewportView(tblTipoNota);
+        if (tblTipoNota.getColumnModel().getColumnCount() > 0) {
+            tblTipoNota.getColumnModel().getColumn(1).setMinWidth(60);
+            tblTipoNota.getColumnModel().getColumn(1).setMaxWidth(60);
+            tblTipoNota.getColumnModel().getColumn(2).setMinWidth(60);
+            tblTipoNota.getColumnModel().getColumn(2).setMaxWidth(60);
+        }
+
+        jLabel1.setText("Carrera:");
+
+        lblNombreCarrera.setText("jLabel2");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jSeparator1)
-            .addComponent(jSeparator2)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCancelar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnGuardar)
+                .addGap(10, 10, 10))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnCancelar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 364, Short.MAX_VALUE)
-                        .addComponent(btnGuardar))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(lblCarrera, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cmdPeriodoLectivo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(cmdPeriodoLectivo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lblNombreCarrera)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -122,13 +135,17 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblCarrera, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(cmdPeriodoLectivo, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(101, 101, 101)
-                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 29, Short.MAX_VALUE)
+                    .addComponent(lblNombreCarrera))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnGuardar)
-                    .addComponent(btnCancelar))
-                .addContainerGap(8, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancelar)
+                    .addComponent(btnGuardar))
+                .addContainerGap())
         );
 
         pack();
@@ -139,8 +156,11 @@ public class FrmTipoNota extends javax.swing.JInternalFrame {
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JComboBox<String> cmdPeriodoLectivo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JLabel lblCarrera;
+    private javax.swing.JLabel lblNombreCarrera;
+    private javax.swing.JTable tblTipoNota;
     // End of variables declaration//GEN-END:variables
 }
