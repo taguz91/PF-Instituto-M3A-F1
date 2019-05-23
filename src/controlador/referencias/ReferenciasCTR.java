335f39c717f5d1462e2f72343b779981a13ff8a2
puyulG <kpaulgc@live.com.mx>
2019-05-23 14:10:18 -0500

    SI HAY ERRORES CULPA DE JHONNY

--- src/controlador/referencias/ReferenciasCTR.java
+++ src/controlador/referencias/ReferenciasCTR.java
@@ -14,7 +14,7 @@ public class ReferenciasCTR {
     private final ConectarDB conecta;
     private final VtnPrincipal vtnPrin;
     private final VtnPrincipalCTR ctrPrin;
-    private String autor, autorCorporativo, titulo, año, ciudad, editor;
+    private String autor, autor2, autor3, autorCorporativo, titulo, año, ciudad, editor;
 
     public ReferenciasCTR(ConectarDB conecta, VtnPrincipalCTR ctrPrin, VtnPrincipal vtnPrin, frmBibliografia frmBibliografia) {
         this.frmBibliografia = frmBibliografia;
@@ -27,7 +27,7 @@ public class ReferenciasCTR {
         frmBibliografia.getCbxExistenciaBiblioteca().setSelected(true);
         frmBibliografia.getTxtCodigoKoha().setEnabled(false);
         frmBibliografia.getTxtCodigoKoha().setText("S/N");
-        
+
     }
 
     public void iniciarControlador() {
@@ -37,7 +37,8 @@ public class ReferenciasCTR {
     }
 
     public void bottonGuardar() {
-        if (frmBibliografia.getTxtAnio().getText().equals(null) || frmBibliografia.getTxtAutor().getText().equals(null)
+        if (frmBibliografia.getTxtAnio().getText().equals(null) || frmBibliografia.getTxtAutor2().getText().equals(null) || frmBibliografia.getTxtAutor3().getText().equals(null)
+                || frmBibliografia.getTxtAutor().getText().equals(null)
                 || frmBibliografia.getTxtCiudad().getText().equals(null) || frmBibliografia.getTxtEditor().getText().equals(null)) {
             JOptionPane.showMessageDialog(null, "Campos vacios no Permitido");
         } else {
@@ -50,19 +51,29 @@ public class ReferenciasCTR {
             String observaciones = frmBibliografia.getTxtObservaciones().getText();
             String tipoD = "Base";
             String contenedor = "";
-            if(frmBibliografia.getCbxAutorCorporativo().isSelected()==true)
-            {
-              autor = frmBibliografia.getTxtAutorCorporativo().getText();
-              frmBibliografia.getTxtAutor().setEnabled(false);
-            }else{
-            autor = frmBibliografia.getTxtAutor().getText();}
-            
+
+            autor = frmBibliografia.getTxtAutor().getText();
+            autor2 = frmBibliografia.getTxtAutor2().getText();
+            autor3 = frmBibliografia.getTxtAutor3().getText();
+
             titulo = frmBibliografia.getTxtTitulo().getText();
             año = frmBibliografia.getTxtAnio().getText();
             ciudad = frmBibliografia.getTxtCiudad().getText();
             editor = frmBibliografia.getTxtEditor().getText();
-            contenedor = autor + ',' + titulo + ',' + año + ',' + ciudad + ',' + editor;
+            if (frmBibliografia.getTxtAutor().getText().length() > 0 && frmBibliografia.getTxtAutor2().getText().length() == 0
+                    && frmBibliografia.getTxtAutor3().getText().length() == 0) {
+                contenedor = autor + '.' + año + '.' + titulo + '.' + editor + '.' + ciudad;
+            } else if (frmBibliografia.getTxtAutor().getText().length() > 0 && frmBibliografia.getTxtAutor2().getText().length() > 0
+                    && frmBibliografia.getTxtAutor3().getText().length() == 0) {
+                contenedor = autor + '&' + autor2 + '.' + año + '.' + titulo + '.' + editor + '.' + ciudad;
+            } else if (frmBibliografia.getTxtAutor().getText().length() > 0 && frmBibliografia.getTxtAutor2().getText().length() == 0
+                    && frmBibliografia.getTxtAutor3().getText().length() > 0) {
+                contenedor = autor + '&' + autor2 + '&' + autor3 + '.' + año + '.' + titulo + '.' + editor + '.' + ciudad;
+            }
+
             BDbibliografia.setCodigo_referencia(frmBibliografia.getTxtCodigoLibro().getText());
+            BDbibliografia.setAutor2(autor2);
+            BDbibliografia.setAutor3(autor3);
             BDbibliografia.setDescripcion_referencia(contenedor);
             BDbibliografia.setTipo_referencia(tipoD);
             BDbibliografia.setExiste_en_biblioteca(existe);
@@ -72,26 +83,25 @@ public class ReferenciasCTR {
             BDbibliografia.setCodigo_koha(frmBibliografia.getTxtCodigoKoha().getText());
             BDbibliografia.setCodigo_dewey(frmBibliografia.getTxtCodigoDewey().getText());
             BDbibliografia.setArea_referencias(frmBibliografia.getTxtArea().getText());
-            
+
             if (BDbibliografia.insertarReferencia()) {
                 JOptionPane.showMessageDialog(null, "Los Datos se guardaron Correctamente");
                 frmBibliografia.getTxtAutor().setText("");
+                frmBibliografia.getTxtAutor2().setText("");
+                frmBibliografia.getTxtAutor3().setText("");
                 frmBibliografia.getTxtTitulo().setText("");
                 frmBibliografia.getTxtAnio().setText("");
                 frmBibliografia.getTxtCiudad().setText("");
                 frmBibliografia.getTxtEditor().setText("");
                 frmBibliografia.getTxtObservaciones().setText("");
                 frmBibliografia.getTxtCodigoLibro().setText("");
-                frmBibliografia.getTxtAutorCorporativo().setText("");
                 frmBibliografia.getTxtCodigoISBM().setText("");
                 frmBibliografia.getTxtNumeroPaginas().setText("");
-                frmBibliografia.getCbxAutorCorporativo().setSelected(false);
                 frmBibliografia.getTxtCodigoKoha().setText("S/N");
                 frmBibliografia.getTxtCodigoDewey().setText("");
                 frmBibliografia.getTxtArea().setText("");
                 frmBibliografia.getTxtAutor().setEnabled(true);
                 frmBibliografia.getTxtCodigoKoha().setEnabled(false);
-                        
 
             } else {
                 JOptionPane.showMessageDialog(null, "ERROR  Datos no guardados");
