d70d4c09c72868fcfb72372b421cd5ac204a0682
JohnnyGG98 <johnnygar98@hotmail.com>
2019-05-23 15:52:33 -0500

    .
    .

--- src/controlador/notas/ReportesCTR.java
+++ src/controlador/notas/ReportesCTR.java
@@ -55,7 +55,7 @@ public class ReportesCTR {
         parametros.put("curso_nombre", ciclo);
         parametros.put("materia_nombre", materia);
 
-        JOptionPane.showMessageDialog(null, getClass().getResource(path).toString());
+        //JOptionPane.showMessageDialog(null, getClass().getResource(path).toString());
 
         Middlewares.generarReporte(getClass().getResource(path), "Reporte Menor 70", parametros);
     }
