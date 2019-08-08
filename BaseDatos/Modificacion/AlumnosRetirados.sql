2a04af21d7d4a4d1049e543baef34000c526d0d0
Gustavo <johnnygar98@hotmail.com>
2019-08-07 19:44:29 -0500

    Mejorando el update

--- BaseDatos/Modificacion/AlumnosRetirados.sql
+++ BaseDatos/Modificacion/AlumnosRetirados.sql
@@ -1,6 +1,6 @@
 --Arreglar los alumnos no matriculados.
 --Todos los alumnos con matricula anulada sin que conste en alumno curso
-SELECT * FROM public."AlumnoCurso"
+SELECT id_almn_curso FROM public."AlumnoCurso"
 WHERE id_almn_curso IN (
 	SELECT id_almn_curso FROM public."AlumnoCursoRetirados"
 	WHERE retiro_activo = true
@@ -10,7 +10,9 @@ WHERE id_almn_curso IN (
 UPDATE public."AlumnoCurso"
 SET almn_curso_activo = false
 WHERE id_almn_curso IN (
-  SELECT id_almn_curso
-  FROM public."AlumnoCursoRetirados"
-	WHERE retiro_activo = true
+  SELECT id_almn_curso FROM public."AlumnoCurso"
+  WHERE id_almn_curso IN (
+  	SELECT id_almn_curso FROM public."AlumnoCursoRetirados"
+  	WHERE retiro_activo = true
+  ) AND almn_curso_activo = true
 )
