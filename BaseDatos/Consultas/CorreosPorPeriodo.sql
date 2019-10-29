4771d9b1fcc31340db5f23e17dc8c094a578bfdb
Johnny <johnnygar98@hotmail.com>
2019-10-28 20:32:31 -0500

    Mejorando la consulta de correos

--- BaseDatos/Consultas/CorreosPorPeriodo.sql
+++ BaseDatos/Consultas/CorreosPorPeriodo.sql
@@ -1,6 +1,9 @@
 SELECT
-persona_correo
-FROM public."Matriculas" m
-JOIN public."Alumnos" a JOIN a.id_alumno = m.id_alumno
-JOIN public."Personas" p JOIN p.id_persona = a.id_persona
+persona_correo AS correos
+FROM public."Matricula" m
+JOIN public."Alumnos" a ON a.id_alumno = m.id_alumno
+JOIN public."Personas" p ON p.id_persona = a.id_persona
 WHERE m.id_prd_lectivo = 21;
+
+
+-- Periodos (TDS 21, TAS 22, TAF 23, TSMI 24, TSE 25, EDTS 26, SP 27, TSPM 28, DII 29)
