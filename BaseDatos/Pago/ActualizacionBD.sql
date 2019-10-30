f8e820b4138c221d86ad10e9669122508201cf96
Johnny <johnnygar98@hotmail.com>
2019-10-30 18:50:05 -0500

    Nuevas tablas BD

--- BaseDatos/Pago/ActualizacionBD.sql
+++ BaseDatos/Pago/ActualizacionBD.sql
@@ -6,13 +6,20 @@ CREATE TABLE pago."Pago"(
   id_pago SERIAL NOT NULL,
   id_comprobante INT NOT NULL,
   id_malla_alumno INT NOT NULL,
-
+  pago NUMERIC(8, 4) NOT NULL DEFAULT '0.0',
+  pago_numero_matricula INT NOT NULL DEFAULT 2,
+  pago_activo BOOLEAN NOT NULL DEFAULT 'false',
+  CONSTRAINT pago_pk PRIMARY KEY("id_pago")
 ) WITH (OIDS = FALSE);
 
 CREATE TABLE pago."Comprobante"(
   id_comprobante SERIAL NOT NULL,
+  id_prd_lectivo INT NOT NULL,
   id_almn_carrera INT NOT NULL,
-  comprobante BYTEA NOT NULL,
+  comprobante BYTEA,
+  comprobante_codigo character varying(50) NOT NULL DEFAULT '',
+  comprobante_fecha_pago TIMESTAMP NOT NULL DEFAULT now(),
+  comprobante_usuario_ingreso character varying(50) NOT NULL DEFAULT 'ADMIN',
   comprobante_activo BOOLEAN NOT NULL DEFAULT 'true',
   CONSTRAINT comprobante_pk PRIMARY KEY("id_comprobante")
 ) WITH (OIDS = FALSE);
