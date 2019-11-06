3fd3bfa86cd5c27d7212598fb54134dbd2ddfdc9
Johnny <johnnygar98@hotmail.com>
2019-11-06 14:51:42 -0500

    Agregando la fecha

--- src/modelo/unidadSilabo/UnidadSilaboBD.java
+++ src/modelo/unidadSilabo/UnidadSilaboBD.java
@@ -1,24 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo.unidadSilabo;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelo.ConexionBD;
-import modelo.evaluacionSilabo.EvaluacionSilabo;
-import modelo.silabo.Silabo;
-import modelo.silabo.SilaboMD;
 
 /**
  *
