d5b8da11d7c63b9f4eba9bc7dd614addb0431aec
Johnny <johnnygar98@hotmail.com>
2019-11-14 22:12:17 -0500

    Por ciclo

--- BaseDatos/proyecto alumnos egresados/queries.sql
+++ BaseDatos/proyecto alumnos egresados/queries.sql
@@ -3,8 +3,8 @@
 	(SACA EL PROMEDIO DE LAS MATERIAS QUE NO SEAN IGUALES AL VARCHAR("materias_filtrar"))
 */
 CREATE OR REPLACE FUNCTION "CALCULAR_PROMEDIO_NOT_SIMILAR_TO"("id_almn_carr" int4, "materias_filtrar" varchar)
-  RETURNS "pg_catalog"."numeric" AS $BODY$ 
-  
+  RETURNS "pg_catalog"."numeric" AS $BODY$
+
   DECLARE
 	malla_cursor CURSOR FOR SELECT
 		"MallaAlumno".malla_almn_nota1,
@@ -12,30 +12,30 @@ CREATE OR REPLACE FUNCTION "CALCULAR_PROMEDIO_NOT_SIMILAR_TO"("id_almn_carr" int
 		"MallaAlumno".malla_almn_nota3
 	FROM
 		"MallaAlumno"
-		INNER JOIN "Materias" ON "MallaAlumno".id_materia = "Materias".id_materia 
+		INNER JOIN "Materias" ON "MallaAlumno".id_materia = "Materias".id_materia
 	WHERE
-		"MallaAlumno".id_almn_carrera = id_almn_carr 
+		"MallaAlumno".id_almn_carrera = id_almn_carr
 		AND "Materias".materia_nombre NOT SIMILAR TO materias_filtrar;
-	
-	
+
+
 	registro RECORD;
 	promedio_total NUMERIC := 0;
 	total_materias INTEGER := 0;
 
 BEGIN-- Routine body goes here...
-	
+
 	OPEN malla_cursor;
-	
+
 	FETCH malla_cursor INTO registro;
-	
+
 	WHILE ( FOUND ) LOOP
-		
-		
+
+
 		IF registro.malla_almn_nota3 <> 0 THEN
 				promedio_total = promedio_total + registro.malla_almn_nota3;
 		ELSE
 
-			IF registro.malla_almn_nota2 <> 0 THEN 
+			IF registro.malla_almn_nota2 <> 0 THEN
 				promedio_total = promedio_total + registro.malla_almn_nota2;
 			ELSE
 				promedio_total = promedio_total + registro.malla_almn_nota1;
@@ -67,8 +67,8 @@ END $BODY$ LANGUAGE plpgsql VOLATILE COST 100;
 	(SACA EL PROMEDIO DE LAS MATERIAS QUE SEAN IGUALES AL VARCHAR("materias_filtrar"))
 */
 CREATE OR REPLACE FUNCTION "CALCULAR_PROMEDIO_SIMILAR_TO"("id_almn_carr" int4, "materias_filtrar" varchar)
-  RETURNS "pg_catalog"."numeric" AS $BODY$ 
-  
+  RETURNS "pg_catalog"."numeric" AS $BODY$
+
   DECLARE
 	malla_cursor CURSOR FOR SELECT
 		"MallaAlumno".malla_almn_nota1,
@@ -76,30 +76,30 @@ CREATE OR REPLACE FUNCTION "CALCULAR_PROMEDIO_SIMILAR_TO"("id_almn_carr" int4, "
 		"MallaAlumno".malla_almn_nota3
 	FROM
 		"MallaAlumno"
-		INNER JOIN "Materias" ON "MallaAlumno".id_materia = "Materias".id_materia 
+		INNER JOIN "Materias" ON "MallaAlumno".id_materia = "Materias".id_materia
 	WHERE
-		"MallaAlumno".id_almn_carrera = id_almn_carr 
+		"MallaAlumno".id_almn_carrera = id_almn_carr
 		AND "Materias".materia_nombre SIMILAR TO materias_filtrar;
-	
-	
+
+
 	registro RECORD;
 	promedio_total NUMERIC := 0;
 	total_materias INTEGER := 0;
 
 BEGIN-- Routine body goes here...
-	
+
 	OPEN malla_cursor;
-	
+
 	FETCH malla_cursor INTO registro;
-	
+
 	WHILE ( FOUND ) LOOP
-		
-		
+
+
 		IF registro.malla_almn_nota3 <> 0 THEN
 				promedio_total = promedio_total + registro.malla_almn_nota3;
 		ELSE
 
-			IF registro.malla_almn_nota2 <> 0 THEN 
+			IF registro.malla_almn_nota2 <> 0 THEN
 				promedio_total = promedio_total + registro.malla_almn_nota2;
 			ELSE
 				promedio_total = promedio_total + registro.malla_almn_nota1;
@@ -140,53 +140,8 @@ FROM
 	"MallaAlumno"
 	INNER JOIN "AlumnosCarrera" ON "MallaAlumno".id_almn_carrera = "AlumnosCarrera".id_almn_carrera
 	INNER JOIN "Alumnos" ON "AlumnosCarrera".id_alumno = "Alumnos".id_alumno
-	INNER JOIN "Personas" ON "Alumnos".id_persona = "Personas".id_persona 
+	INNER JOIN "Personas" ON "Alumnos".id_persona = "Personas".id_persona
 WHERE
-	"AlumnosCarrera".id_carrera = 3 
-	AND "MallaAlumno".malla_almn_ciclo = 5 
+	"AlumnosCarrera".id_carrera = 3
+	AND "MallaAlumno".malla_almn_ciclo = 5
 	AND "MallaAlumno".malla_almn_estado = 'C'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
