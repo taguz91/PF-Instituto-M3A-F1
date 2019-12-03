57bc3e7d89b75bc62306407e88fe9cd2fc17003c
MrRainx <diegocondo10@yahoo.es>
2019-12-03 16:05:03 -0500

    agregue los reportes desde Java

--- src/modelo/ConnDBPool.java
+++ src/modelo/ConnDBPool.java
@@ -11,9 +11,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
 
 /**
@@ -92,7 +94,7 @@ public class ConnDBPool {
             }
 
             stmt.executeUpdate();
-            
+
             return null;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -233,11 +235,8 @@ public class ConnDBPool {
     public ResultSet ejecutarQuery(String sql, Map<Integer, Object> parametros) {
         Connection conn = getConnection();
         try {
-            if (parametros == null) {
-                stmt = conn.prepareStatement(sql);
-            } else {
-                stmt = prepararStatement(sql, conn, parametros);
-            }
+
+            stmt = prepararStatement(sql, conn, parametros);
             rs = stmt.executeQuery();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -405,6 +404,14 @@ public class ConnDBPool {
             );
         }
         return noSQLPOOL(ps);
+    }
+
+    public String useINsql(List<Integer> ids, String statement) {
+        String sqlIn = ids.stream()
+                .map(x -> String.valueOf(x))
+                .collect(Collectors.joining(",", "(", ")"));
+
+        return statement.replace("(?)", sqlIn);
     }
 
 }
